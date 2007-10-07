a44089aa9c97027ef44b8f29bfbab015c3715621
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2007-10-07 13:27:04 +0000

    Changing pckages to geovista.x

--- geoviz/src/main/java/geovista/geoviz/descriptive/DescriptiveStats.java
+++ geoviz/src/main/java/geovista/geoviz/descriptive/DescriptiveStats.java
@@ -19,7 +19,7 @@
  License along with this library; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  -------------------------------------------------------------------   */
-package edu.psu.geovista.geoviz.descriptive;
+package geovista.geoviz.descriptive;
 
 import java.awt.BorderLayout;
 import java.text.DecimalFormat;
