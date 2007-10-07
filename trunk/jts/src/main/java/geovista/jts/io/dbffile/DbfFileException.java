e50ea19f49da74e4e3813bd96f5a34145cb9a6f3
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2007-10-07 01:30:00 +0000

    changing edu.psu.geovista.x to geovista.x

---
+++ trunk/jts/src/main/java/geovista/jts/io/dbffile/DbfFileException.java
@@ -0,0 +1,14 @@
+package geovista.jts.io.dbffile;
+/**
+ * Thrown when an error relating to the shapefile
+ * occures
+ */
+public class DbfFileException extends Exception{
+    public DbfFileException(String s){
+        super(s);
+    }
+}
+
+
+
+
