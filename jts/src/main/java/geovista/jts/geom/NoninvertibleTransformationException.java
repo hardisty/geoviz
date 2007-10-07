5a7a5a1efc4e376f321d9b9176219cea37887b23
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2007-10-07 01:30:00 +0000

    changing edu.psu.geovista.x to geovista.x

---
+++ jts/src/main/java/geovista/jts/geom/NoninvertibleTransformationException.java
@@ -0,0 +1,20 @@
+package geovista.jts.geom;
+
+/**
+ * Indicates that an {@link AffineTransformation}
+ * is non-invertible.
+ * 
+ * @author Martin Davis
+ */
+public class NoninvertibleTransformationException
+	extends Exception
+{
+  public NoninvertibleTransformationException()
+  {
+    super();
+  }
+  public NoninvertibleTransformationException(String msg)
+  {
+    super(msg);
+  }
+}
---
+++ jts/src/main/java/geovista/jts/io/shapefile/ShapeHandler.java
@@ -0,0 +1,13 @@
+package geovista.jts.io.shapefile;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+
+import geovista.jts.io.EndianDataInputStream;
+import geovista.jts.io.EndianDataOutputStream;
+
+public interface ShapeHandler {
+    public int getShapeType();
+    public Geometry read(EndianDataInputStream file,GeometryFactory geometryFactory,int contentLength) throws java.io.IOException,InvalidShapefileException;
+    public void write(Geometry geometry,EndianDataOutputStream file) throws java.io.IOException;
+    public int getLength(Geometry geometry); //length in 16bit words
+}
