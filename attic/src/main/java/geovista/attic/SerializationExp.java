fb445f287c6ad5f1bb284664a9919d6ee8db30c8
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2007-10-14 01:48:56 +0000

    adding attic

---
+++ attic/src/main/java/geovista/attic/SerializationExp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package geovista.attic;
+
+/**
+ * @author hardisty
+ *
+ */
+public class SerializationExp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
