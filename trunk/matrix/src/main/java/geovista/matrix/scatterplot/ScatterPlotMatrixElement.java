b3d7b706dad4d25ecb0ca40c988d2fb48c575bd4
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2008-02-06 03:48:55 +0000

    moved scatterplot classes up to geoviz package

---
+++ trunk/matrix/src/main/java/geovista/matrix/scatterplot/ScatterPlotMatrixElement.java
@@ -0,0 +1,36 @@
+package  geovista.matrix.scatterplot;
+
+/**
+ * Title: ScatterPlot
+ * Description: construct a scatterplot
+ * Copyright:    Copyright (c) 2001
+ * Company: GeoVISTA Center
+ * @author Xiping Dai
+ * @author Jin Chen
+ * @version 1.0
+ */
+import java.awt.Color;
+import java.awt.Graphics;
+
+import geovista.geoviz.scatterplot.ScatterPlotBasic;
+import geovista.matrix.MatrixElement;
+import geovista.symbolization.BivariateColorSymbolClassification;
+
+/**
+ * put your documentation comment here
+ */
+public class ScatterPlotMatrixElement extends ScatterPlotBasic implements MatrixElement{
+
+
+	/**
+	 * Return itself.
+	 * 
+	 * @return
+	 */
+	public MatrixElement getThis() {
+		return this;
+	}
+}
+
+
+
