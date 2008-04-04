b5997d7844b3c90cbf4045be299e0d34cc598cc5
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2008-04-04 01:02:50 +0000

    using autocleanup tools

--- sound/src/test/java/geovista/sound/SonicClassifierTest.java
+++ sound/src/test/java/geovista/sound/SonicClassifierTest.java
@@ -1,25 +1,16 @@
-/* -------------------------------------------------------------------
- GeoVISTA Center (Penn State, Dept. of Geography)
- Java source file for the class SonicRampTest
- Copyright (c), 2000, GeoVISTA Center
- All Rights Reserved.
- Original Author: Frank Hardisty
- $Author: jmacgill $
- $Id: SonicRampTest.java,v 1.1 2003/07/29 15:47:31 jmacgill Exp $
- $Date: 2003/07/29 15:47:31 $
- Reference:		Document no:
- ___				___
- -------------------------------------------------------------------   */
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
 
 package geovista.sound;
 
-import geovista.coordination.CoordinationManager;
-import geovista.geoviz.sample.GeoData48States;
-import geovista.geoviz.sample.GeoDataGeneralizedStates;
-import geovista.geoviz.shapefile.ShapeFileToShape;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import geovista.coordination.CoordinationManager;
+import geovista.geoviz.sample.GeoDataGeneralizedStates;
+import geovista.geoviz.shapefile.ShapeFileToShape;
 
 public class SonicClassifierTest extends TestCase {
 	protected SonicClassifier sClasser;
--- sound/src/test/java/geovista/sound/SonicRampTest.java
+++ sound/src/test/java/geovista/sound/SonicRampTest.java
@@ -1,16 +1,6 @@
-/* -------------------------------------------------------------------
- GeoVISTA Center (Penn State, Dept. of Geography)
- Java source file for the class SonicRampTest
- Copyright (c), 2000, GeoVISTA Center
- All Rights Reserved.
- Original Author: Frank Hardisty
- $Author: jmacgill $
- $Id: SonicRampTest.java,v 1.1 2003/07/29 15:47:31 jmacgill Exp $
- $Date: 2003/07/29 15:47:31 $
- Reference:		Document no:
- ___				___
- -------------------------------------------------------------------   */
-
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
 
 package geovista.sound;
 
@@ -20,62 +10,59 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-
 public class SonicRampTest extends TestCase {
 	protected SonicRamp ramp;
 
 	public SonicRampTest(String name) {
 		super(name);
-                setUp();
+		setUp();
 	}
 
+	@Override
 	protected void setUp() {
 		ramp = new SonicRamp();
-                Color[] colors = new Color[3];
-                colors[0] = Color.white;
-                colors[2] = Color.black;
-                ramp.rampColors(colors);
+		Color[] colors = new Color[3];
+		colors[0] = Color.white;
+		colors[2] = Color.black;
+		ramp.rampColors(colors);
 	}
+
 	public static Test suite() {
 		return new TestSuite(SonicRampTest.class);
 	}
 
-
 	public void testGetRampedValueRGB() {
-            double prop;
+		double prop;
 
-            int resultColor;
+		int resultColor;
 
-            prop = 0;
+		prop = 0;
 
-            resultColor = ramp.getRampedValueRGB(prop);
-            assertTrue(resultColor == -1);
+		resultColor = ramp.getRampedValueRGB(prop);
+		assertTrue(resultColor == -1);
 
 	}
 
-	
 	public void testRampColors() {
-            boolean[] anch = new boolean[7];
-            //all anchors start out false
-            anch[0] = true;
-            anch[2] = true;
-            anch[6] = true;
+		boolean[] anch = new boolean[7];
+		// all anchors start out false
+		anch[0] = true;
+		anch[2] = true;
+		anch[6] = true;
 
-            Color[] col = new Color[7];
-            for (int i = 0; i < col.length; i++) {
-              col[i] = Color.white;
-            }
-            col[6] = Color.black;
+		Color[] col = new Color[7];
+		for (int i = 0; i < col.length; i++) {
+			col[i] = Color.white;
+		}
+		col[6] = Color.black;
 
-            ramp.rampColors(col,anch);
-            int redVal = col[1].getRed();
-            assertTrue(col[1].getRed() == 255);
-            redVal = col[5].getRed();
-            //255 * .25 = 63.75
-            assertTrue(redVal == 64);
-
+		ramp.rampColors(col, anch);
+		int redVal = col[1].getRed();
+		assertTrue(col[1].getRed() == 255);
+		redVal = col[5].getRed();
+		// 255 * .25 = 63.75
+		assertTrue(redVal == 64);
 
 	}
-
 
 }
