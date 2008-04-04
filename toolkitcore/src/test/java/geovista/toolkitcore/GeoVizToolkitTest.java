6b6745c7808b8451c5c9193ec38e459dfc146f77
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2008-04-04 01:00:41 +0000

    using autocleanup tools

--- toolkitcore/src/test/java/geovista/toolkitcore/GeoVizToolkitTest.java
+++ toolkitcore/src/test/java/geovista/toolkitcore/GeoVizToolkitTest.java
@@ -1,43 +1,44 @@
-/*
- * A test class for GeoViz Toolkit.
- */
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
+
 package geovista.toolkitcore;
 
 import junit.framework.TestCase;
 
 public class GeoVizToolkitTest extends TestCase {
 
-	//;// = new GeoVizToolkit("");
+	// ;// = new GeoVizToolkit("");
 	public GeoVizToolkitTest(String name) {
 		super(name);
 	}
 
-//	protected void setUp() throws Exception {
-//		newKit = new GeoVizToolkit("");
-//		newKit.setVisible(true);
-//		super.setUp();
-//	}
+	// protected void setUp() throws Exception {
+	// newKit = new GeoVizToolkit("");
+	// newKit.setVisible(true);
+	// super.setUp();
+	// }
 
 	public void testMain() {
-		
-		
+
 		System.out.println("I did done test main!");
 
 	}
-	public void testRemoveAllBeans(){
-//		GeoVizToolkit newKit = new GeoVizToolkit("");
-//		int numBeans = newKit.coord.getFiringBeans().length;
-//		System.out.println(numBeans);
-//		newKit.removeAllBeans();
-//		numBeans = newKit.coord.getFiringBeans().length;
-//		System.out.println(numBeans);		
-//		assert(newKit.coord.getFiringBeans().length == 0);
-		
-		
+
+	public void testRemoveAllBeans() {
+		// GeoVizToolkit newKit = new GeoVizToolkit("");
+		// int numBeans = newKit.coord.getFiringBeans().length;
+		// System.out.println(numBeans);
+		// newKit.removeAllBeans();
+		// numBeans = newKit.coord.getFiringBeans().length;
+		// System.out.println(numBeans);
+		// assert(newKit.coord.getFiringBeans().length == 0);
+
 	}
-	public void testOpenAllComponents(){
+
+	public void testOpenAllComponents() {
 		GeoVizToolkit newKit = new GeoVizToolkit("");
-		//newKit.openAllComponents();
+		// newKit.openAllComponents();
 		newKit = null;
 	}
 
