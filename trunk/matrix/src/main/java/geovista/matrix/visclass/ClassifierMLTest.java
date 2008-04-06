9ecbcd8b22f6b3a2b4bd14ae18f5a9a3fd74e0d6
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2008-04-06 19:17:46 +0000

    using autocleanup tools Changed to new-style header comments (just LGPL + original author info)

--- trunk/matrix/src/main/java/geovista/matrix/visclass/ClassifierMLTest.java
+++ trunk/matrix/src/main/java/geovista/matrix/visclass/ClassifierMLTest.java
@@ -1,13 +1,8 @@
-package geovista.matrix.visclass;
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Xiping Dai */
 
-/**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2001</p>
- * <p>Company: </p>
- * @author unascribed
- * @version 1.0
- */
+package geovista.matrix.visclass;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -19,69 +14,71 @@ import geovista.matrix.UniPlotMatrix;
 
 public class ClassifierMLTest {
 
-    public ClassifierMLTest() {
-    }
-
-	public static void main (String[] args) {
-
-	JFrame frame = new JFrame("ML Classifier Test");
-	UniPlotMatrix scatterPlotMatrix = new UniPlotMatrix();
-	scatterPlotMatrix.setElementClassName("geovista.geoviz.scatterplot.ScatterPlot");
-
-	geovista.readers.csv.CSVFileDataReader csvReader = new geovista.readers.csv.CSVFileDataReader();
-	String fn = "T:\\GeoVISTA\\data\\kioloa_london.csv";
-	//String fn = "T:\\GeoVISTA\\data\\tm24.csv";
-	//String fn = "D:\\temp\\datatest\\testdata.csv";
-	csvReader.setFileName(fn);
-	Object[] trainingDataSet = csvReader.getDataSet();
-	//Object[] dataSet = new Object[trainingDataSet.length-1];
-	//for (int i = 0; i < dataSet.length; i ++){
-	//	dataSet[i] = trainingDataSet[i];
-	//}
-
-	ClassifierMaximumLikelihood classifierML = new ClassifierMaximumLikelihood();
-	//classifierML.setTrainingData(trainingDataSet);
-	classifierML.setClassNumber(5);
-	DataSetForApps dataSet = new DataSetForApps(trainingDataSet);
-	classifierML.setDataSet(dataSet);
-
-	int[] classification;
-	classification = classifierML.getClassificaiton();
-
-	Object[] dataObject = new Object[trainingDataSet.length + 1];
-	String[] trainingAttributes = (String[])(trainingDataSet[0]);
-	String[] att = new String[trainingAttributes.length + 1];
-	for (int i = 0; i < trainingAttributes.length; i ++){
-		att[i] = trainingAttributes[i];
+	public ClassifierMLTest() {
 	}
-	att[trainingAttributes.length] = "class";
-	dataObject[0] = att;
-	for (int i = 1; i < trainingDataSet.length; i ++){
-		dataObject[i] = trainingDataSet[i];
-	}
-	dataObject[trainingDataSet.length] = classification;
 
-	//ConditionManager cm = new ConditionManager();
-	//cm.setDataObject(dataSet);
-	//int[] cond = cm.getConditionResults();
-	//scatterPlotMatrix.setConditionArray(cond);
-	DataSetForApps dataSet2 = new DataSetForApps(dataObject);
-	scatterPlotMatrix.setDataSet(dataSet2);
+	public static void main(String[] args) {
 
-	frame.addWindowListener(new WindowAdapter() {
-		public void windowClosing(WindowEvent e) {
-			System.exit(0) ;
+		JFrame frame = new JFrame("ML Classifier Test");
+		UniPlotMatrix scatterPlotMatrix = new UniPlotMatrix();
+		scatterPlotMatrix
+				.setElementClassName("geovista.geoviz.scatterplot.ScatterPlot");
+
+		geovista.readers.csv.CSVFileDataReader csvReader = new geovista.readers.csv.CSVFileDataReader();
+		String fn = "T:\\GeoVISTA\\data\\kioloa_london.csv";
+		// String fn = "T:\\GeoVISTA\\data\\tm24.csv";
+		// String fn = "D:\\temp\\datatest\\testdata.csv";
+		csvReader.setFileName(fn);
+		Object[] trainingDataSet = csvReader.getDataSet();
+		// Object[] dataSet = new Object[trainingDataSet.length-1];
+		// for (int i = 0; i < dataSet.length; i ++){
+		// dataSet[i] = trainingDataSet[i];
+		// }
+
+		ClassifierMaximumLikelihood classifierML = new ClassifierMaximumLikelihood();
+		// classifierML.setTrainingData(trainingDataSet);
+		classifierML.setClassNumber(5);
+		DataSetForApps dataSet = new DataSetForApps(trainingDataSet);
+		classifierML.setDataSet(dataSet);
+
+		int[] classification;
+		classification = classifierML.getClassificaiton();
+
+		Object[] dataObject = new Object[trainingDataSet.length + 1];
+		String[] trainingAttributes = (String[]) (trainingDataSet[0]);
+		String[] att = new String[trainingAttributes.length + 1];
+		for (int i = 0; i < trainingAttributes.length; i++) {
+			att[i] = trainingAttributes[i];
 		}
-	});
-	//scatterPlotMatrix.invokedStandalone = true;
-	frame.getContentPane().add(scatterPlotMatrix);
-	frame.setLocation(400, 400);
-	frame.setSize(600, 600);
-	frame.setVisible(true);
-	//frame.repaint();
-	frame.setVisible(true);
-	//scatterPlotMatrix.setDoubleDataArrays(dataValue2);
-	frame.pack();
+		att[trainingAttributes.length] = "class";
+		dataObject[0] = att;
+		for (int i = 1; i < trainingDataSet.length; i++) {
+			dataObject[i] = trainingDataSet[i];
+		}
+		dataObject[trainingDataSet.length] = classification;
+
+		// ConditionManager cm = new ConditionManager();
+		// cm.setDataObject(dataSet);
+		// int[] cond = cm.getConditionResults();
+		// scatterPlotMatrix.setConditionArray(cond);
+		DataSetForApps dataSet2 = new DataSetForApps(dataObject);
+		scatterPlotMatrix.setDataSet(dataSet2);
+
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		// scatterPlotMatrix.invokedStandalone = true;
+		frame.getContentPane().add(scatterPlotMatrix);
+		frame.setLocation(400, 400);
+		frame.setSize(600, 600);
+		frame.setVisible(true);
+		// frame.repaint();
+		frame.setVisible(true);
+		// scatterPlotMatrix.setDoubleDataArrays(dataValue2);
+		frame.pack();
 	}
 
 }
--- trunk/matrix/src/main/java/geovista/matrix/visclass/VisualClassifierMultiVariablesBeanInfo.java
+++ trunk/matrix/src/main/java/geovista/matrix/visclass/VisualClassifierMultiVariablesBeanInfo.java
@@ -1,3 +1,7 @@
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Xiping Dai */
+
 package geovista.matrix.visclass;
 
 import java.beans.BeanInfo;
@@ -6,52 +10,48 @@ import java.beans.SimpleBeanInfo;
 
 import geovista.geoviz.visclass.VisualClassifier;
 
-/**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2002</p>
- * <p>Company: </p>
- * @author unascribed
- * @version 1.0
- */
-
 public class VisualClassifierMultiVariablesBeanInfo extends SimpleBeanInfo {
-  static Class beanClass = VisualClassifier.class;
-  static String iconColor16x16Filename = "resources/visualClassifier16.gif";
-  static String iconColor32x32Filename = "resources/visualClassifier32.gif";
-  static String iconMono16x16Filename;
-  static String iconMono32x32Filename;
+	static Class beanClass = VisualClassifier.class;
+	static String iconColor16x16Filename = "resources/visualClassifier16.gif";
+	static String iconColor32x32Filename = "resources/visualClassifier32.gif";
+	static String iconMono16x16Filename;
+	static String iconMono32x32Filename;
 
-  public VisualClassifierMultiVariablesBeanInfo() {
-  }
+	public VisualClassifierMultiVariablesBeanInfo() {
+	}
 
-     public PropertyDescriptor[] getPropertyDescriptors() {
+	@Override
+	public PropertyDescriptor[] getPropertyDescriptors() {
 		try {
 			return (new PropertyDescriptor[] {
-				new PropertyDescriptor("symbolizationPanel", beanClass),
-                                new PropertyDescriptor("variableChooserMode", beanClass),
-                                new PropertyDescriptor("colors", beanClass)//,
-                                //new PropertyDescriptor("labelTable", beanClass),
+					new PropertyDescriptor("symbolizationPanel", beanClass),
+					new PropertyDescriptor("variableChooserMode", beanClass),
+					new PropertyDescriptor("colors", beanClass) // ,
+			// new PropertyDescriptor("labelTable", beanClass),
 			});
 		} catch (Exception e) {
-                        e.printStackTrace();
-			return new PropertyDescriptor[]{};
+			e.printStackTrace();
+			return new PropertyDescriptor[] {};
 		}
-    }
+	}
 
-
-  public java.awt.Image getIcon(int iconKind) {
-    switch (iconKind) {
-    case BeanInfo.ICON_COLOR_16x16:
-        return iconColor16x16Filename != null ? loadImage(iconColor16x16Filename) : null;
-    case BeanInfo.ICON_COLOR_32x32:
-        return iconColor32x32Filename != null ? loadImage(iconColor32x32Filename) : null;
-    case BeanInfo.ICON_MONO_16x16:
-        return iconMono16x16Filename != null ? loadImage(iconMono16x16Filename) : null;
-    case BeanInfo.ICON_MONO_32x32:
-        return iconMono32x32Filename != null ? loadImage(iconMono32x32Filename) : null;
-        }
-    return null;
-  }
+	@Override
+	public java.awt.Image getIcon(int iconKind) {
+		switch (iconKind) {
+		case BeanInfo.ICON_COLOR_16x16:
+			return iconColor16x16Filename != null
+					? loadImage(iconColor16x16Filename) : null;
+		case BeanInfo.ICON_COLOR_32x32:
+			return iconColor32x32Filename != null
+					? loadImage(iconColor32x32Filename) : null;
+		case BeanInfo.ICON_MONO_16x16:
+			return iconMono16x16Filename != null
+					? loadImage(iconMono16x16Filename) : null;
+		case BeanInfo.ICON_MONO_32x32:
+			return iconMono32x32Filename != null
+					? loadImage(iconMono32x32Filename) : null;
+		}
+		return null;
+	}
 
 }
