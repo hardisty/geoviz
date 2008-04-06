89e3292fe70669c01215347cf4f6634e88a232d7
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2008-04-06 19:12:00 +0000

    using autocleanup tools Changed to new-style header comments (just LGPL + original author info)

--- trunk/matrix/src/main/java/geovista/matrix/spacefill/SpaceFillMatrixElement.java
+++ trunk/matrix/src/main/java/geovista/matrix/spacefill/SpaceFillMatrixElement.java
@@ -1,21 +1,8 @@
-/* -------------------------------------------------------------------
- GeoVISTA Center (Penn State, Dept. of Geography)
- Java source file for the class SpaceFillMatrixElement
- Copyright (c), 2002, GeoVISTA Center
- All Rights Reserved.
- Original Author: Frank Hardisty
- $Author: xpdai $
- $Id: SpaceFillMatrixElement.java,v 1.5 2005/01/04 19:17:20 xpdai Exp $
- $Date: 2005/01/04 19:17:20 $
- Reference:		Document no:
- ___				___
- -------------------------------------------------------------------  *
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
 
- */
-
-
-package  geovista.matrix.spacefill;
-
+package geovista.matrix.spacefill;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -32,142 +19,149 @@ import geovista.common.data.DataSetForApps;
 import geovista.geoviz.spacefill.SpaceFillCanvas;
 import geovista.matrix.MatrixElement;
 
-public class SpaceFillMatrixElement extends SpaceFillCanvas
-                                                  implements MatrixElement {
+public class SpaceFillMatrixElement extends SpaceFillCanvas implements
+		MatrixElement {
 
-        //the following are required for returning to matrix
-        private int[] elementPosition;
-        private double[] xAxisExtents;
-        private double[] yAxisExtents;
-        private DataSetForApps dataSet;
-		private Color selectionColor;
+	// the following are required for returning to matrix
+	private int[] elementPosition;
+	private double[] xAxisExtents;
+	private double[] yAxisExtents;
+	private DataSetForApps dataSet;
+	private Color selectionColor;
 
-        public SpaceFillMatrixElement() {
-          super();
-          super.setUseDrawingShapes(false);
-          this.setBorder(BorderFactory.createLineBorder(Color.darkGray,1));
-        }
-
-        /**
-         * @param data
-         * 
-         * This method is deprecated becuase it wants to create its very own pet
-         * DataSetForApps. This is no longer allowed, to allow for a mutable, 
-         * common data set. Use of this method may lead to unexpected
-         * program behavoir. 
-         * Please use setDataSet instead.
-         */
-        @Deprecated
-        public void setDataObject(Object[] data) {
-      	 this.setDataSet(new DataSetForApps(data));
-          
-        }
-        public void setDataSet(DataSetForApps data){
-          this.dataSet = data;
-          super.setDataSet(data);
-        }
-
-	public void setElementPosition(int[] dataIndices){
-            this.elementPosition = (int[])dataIndices.clone();
-            super.setCurrOrderColumn(this.elementPosition[0]);//order = x
-            super.setCurrColorColumn(this.elementPosition[1]);//color = y
-
-
-        }
-
-        public int[] getElementPosition(){
-          return this.elementPosition;
-        }
-
-	//For axes of scatter plot.
-        //a noop for this class
-	public void setAxisOn (boolean axisOn){
-         }
-
-
-
-
-	//Set min and max for axes. xAxisExtents[0] = min, [1] = max.
-	public void setXAxisExtents (double[] xAxisExtents){
-
-        }
-
-	public void setYAxisExtents (double[] yAxisExtents){  }
-
-	public double[] getXAxisExtents () {
-          return this.xAxisExtents;
-        }
-
-	public double[] getYAxisExtents () {
-          return this.yAxisExtents;
-        }
-
-	public String getShortDiscription () {
-	    return "SFP";
-	}
-    //public void setBivarColorClasser (BivariateColorSymbolClassification bivarColorClasser) {
-      //this.bivarColorClasser = bivarColorClasser;
-      //this.sendColorsToLayers(this.dataColorX.length);
-    //}
-
-	public void setSelectionColor (Color c){
-	    this.selectionColor = c;
-            super.setColorSelection(c);
+	public SpaceFillMatrixElement() {
+		super();
+		super.setUseDrawingShapes(false);
+		setBorder(BorderFactory.createLineBorder(Color.darkGray, 1));
 	}
 
-	public Color getSelectionColor (){
-	    return this.selectionColor;
+	/**
+	 * @param data
+	 * 
+	 * This method is deprecated becuase it wants to create its very own pet
+	 * DataSetForApps. This is no longer allowed, to allow for a mutable, common
+	 * data set. Use of this method may lead to unexpected program behavoir.
+	 * Please use setDataSet instead.
+	 */
+	@Deprecated
+	public void setDataObject(Object[] data) {
+		setDataSet(new DataSetForApps(data));
+
 	}
 
-	public void setMultipleSelectionColors (Color[] c){
+	@Override
+	public void setDataSet(DataSetForApps data) {
+		dataSet = data;
+		super.setDataSet(data);
 	}
 
-	public void setColorArrayForObs (Color[] c){
+	public void setElementPosition(int[] dataIndices) {
+		elementPosition = dataIndices.clone();
+		super.setCurrOrderColumn(elementPosition[0]);// order = x
+		super.setCurrColorColumn(elementPosition[1]);// color = y
+
 	}
 
-        public JToolBar getTools (){
-          return null;
-        }
-    /**
-     * This method only paints the current contents of the drawingBuff.
-     * @param g
-     */
-	public void paintComponent (Graphics g) {
-            super.paintComponent(g);
+	public int[] getElementPosition() {
+		return elementPosition;
+	}
 
-            if (this.elementPosition == null) {
-              return;
-            }
+	// For axes of scatter plot.
+	// a noop for this class
+	public void setAxisOn(boolean axisOn) {
+	}
 
-            if (this.elementPosition[0] == this.elementPosition[1]) {
-              Graphics2D g2 = (Graphics2D)g;
-              Color half = new Color(255,255,255,150);
-              g2.setColor(half);
-              Font font = new Font("Serif",Font.PLAIN,24);
-              g2.setFont(font);
-              FontRenderContext frc = g2.getFontRenderContext();
+	// Set min and max for axes. xAxisExtents[0] = min, [1] = max.
+	public void setXAxisExtents(double[] xAxisExtents) {
 
-              String[] varNames = this.dataSet.getAttributeNamesNumeric();//XXX right method call?
-              String varName = varNames[this.elementPosition[0]-1];//-1 to skip string array
+	}
 
-              int midX = 10;
-              int midY = this.getHeight()/2;
+	public void setYAxisExtents(double[] yAxisExtents) {
+	}
 
-              Rectangle2D textBounds = g2.getFont().getStringBounds(varName,frc);
-              Rectangle rect = textBounds.getBounds();
-              rect.setSize(rect.width,(int)(rect.height * 1.5));
-              rect.setLocation(midX,midY - (int)(rect.getHeight()/1.5));
-              //Rectangle rect = new Rectangle();
-              rect.setLocation(0,0);
-              rect.setSize(this.getWidth(),this.getHeight());
-              g2.fill(rect);
-              //g2.fillRect(0,0,this.getWidth(),this.getHeight());
-              g2.setColor(Color.black);
+	public double[] getXAxisExtents() {
+		return xAxisExtents;
+	}
 
+	public double[] getYAxisExtents() {
+		return yAxisExtents;
+	}
 
+	public String getShortDiscription() {
+		return "SFP";
+	}
 
-              //g2.drawString(varName,midX,midY);
-            }
-        }
+	// public void setBivarColorClasser (BivariateColorSymbolClassification
+	// bivarColorClasser) {
+	// this.bivarColorClasser = bivarColorClasser;
+	// this.sendColorsToLayers(this.dataColorX.length);
+	// }
+
+	public void setSelectionColor(Color c) {
+		selectionColor = c;
+		super.setColorSelection(c);
+	}
+
+	public Color getSelectionColor() {
+		return selectionColor;
+	}
+
+	public void setMultipleSelectionColors(Color[] c) {
+	}
+
+	public void setColorArrayForObs(Color[] c) {
+	}
+
+	public JToolBar getTools() {
+		return null;
+	}
+
+	/**
+	 * This method only paints the current contents of the drawingBuff.
+	 * 
+	 * @param g
+	 */
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		if (elementPosition == null) {
+			return;
+		}
+
+		if (elementPosition[0] == elementPosition[1]) {
+			Graphics2D g2 = (Graphics2D) g;
+			Color half = new Color(255, 255, 255, 150);
+			g2.setColor(half);
+			Font font = new Font("Serif", Font.PLAIN, 24);
+			g2.setFont(font);
+			FontRenderContext frc = g2.getFontRenderContext();
+
+			String[] varNames = dataSet.getAttributeNamesNumeric();// XXX
+			// right
+			// method
+			// call?
+			String varName = varNames[elementPosition[0] - 1];// -1 to
+			// skip
+			// string
+			// array
+
+			int midX = 10;
+			int midY = getHeight() / 2;
+
+			Rectangle2D textBounds = g2.getFont().getStringBounds(varName, frc);
+			Rectangle rect = textBounds.getBounds();
+			rect.setSize(rect.width, (int) (rect.height * 1.5));
+			rect.setLocation(midX, midY - (int) (rect.getHeight() / 1.5));
+			// Rectangle rect = new Rectangle();
+			rect.setLocation(0, 0);
+			rect.setSize(getWidth(), getHeight());
+			g2.fill(rect);
+			// g2.fillRect(0,0,this.getWidth(),this.getHeight());
+			g2.setColor(Color.black);
+
+			// g2.drawString(varName,midX,midY);
+		}
+	}
 
 }
--- trunk/matrix/src/main/java/geovista/matrix/spacefill/SpaceFillMatrixElementBeanInfo.java
+++ trunk/matrix/src/main/java/geovista/matrix/spacefill/SpaceFillMatrixElementBeanInfo.java
@@ -1,3 +1,7 @@
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
+
 package geovista.matrix.spacefill;
 
 import java.beans.BeanInfo;
@@ -5,63 +9,59 @@ import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.beans.SimpleBeanInfo;
 
-/**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2002</p>
- * <p>Company: </p>
- * @author unascribed
- * @version 1.0
- */
-
 public class SpaceFillMatrixElementBeanInfo extends SimpleBeanInfo {
-  static Class beanClass = SpaceFillMatrixElement.class;
-  static String iconColor16x16Filename = "resources/spacefillElement16.gif";
-  static String iconColor32x32Filename = "resources/spacefillElement32.gif";
-  static String iconMono16x16Filename;
-  static String iconMono32x32Filename;
+	static Class beanClass = SpaceFillMatrixElement.class;
+	static String iconColor16x16Filename = "resources/spacefillElement16.gif";
+	static String iconColor32x32Filename = "resources/spacefillElement32.gif";
+	static String iconMono16x16Filename;
+	static String iconMono32x32Filename;
 
-  public SpaceFillMatrixElementBeanInfo() {
-  }
-  public PropertyDescriptor[] getPropertyDescriptors() {
-    try {
-      PropertyDescriptor _axisOn = new PropertyDescriptor("axisOn", beanClass, null, "setAxisOn");
-      PropertyDescriptor _conditionArray = new PropertyDescriptor("conditionArray", beanClass, null, "setConditionArray");
-      PropertyDescriptor _dataObject = new PropertyDescriptor("dataObject", beanClass, null, "setDataObject");
-      PropertyDescriptor _elementPosition = new PropertyDescriptor("elementPosition", beanClass, "getElementPosition", "setElementPosition");
-      PropertyDescriptor _xAxisExtents = new PropertyDescriptor("xAxisExtents", beanClass, null, null);
-      PropertyDescriptor _yAxisExtents = new PropertyDescriptor("yAxisExtents", beanClass, null, null);
-      PropertyDescriptor[] pds = new PropertyDescriptor[] {
-	      _axisOn,
-	      _conditionArray,
-	      _dataObject,
-	      _elementPosition,
-	      _xAxisExtents,
-	      _yAxisExtents};
-      return pds;
+	public SpaceFillMatrixElementBeanInfo() {
+	}
 
+	@Override
+	public PropertyDescriptor[] getPropertyDescriptors() {
+		try {
+			PropertyDescriptor _axisOn = new PropertyDescriptor("axisOn",
+					beanClass, null, "setAxisOn");
+			PropertyDescriptor _conditionArray = new PropertyDescriptor(
+					"conditionArray", beanClass, null, "setConditionArray");
+			PropertyDescriptor _dataObject = new PropertyDescriptor(
+					"dataObject", beanClass, null, "setDataObject");
+			PropertyDescriptor _elementPosition = new PropertyDescriptor(
+					"elementPosition", beanClass, "getElementPosition",
+					"setElementPosition");
+			PropertyDescriptor _xAxisExtents = new PropertyDescriptor(
+					"xAxisExtents", beanClass, null, null);
+			PropertyDescriptor _yAxisExtents = new PropertyDescriptor(
+					"yAxisExtents", beanClass, null, null);
+			PropertyDescriptor[] pds = new PropertyDescriptor[] { _axisOn,
+					_conditionArray, _dataObject, _elementPosition,
+					_xAxisExtents, _yAxisExtents };
+			return pds;
 
+		} catch (IntrospectionException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
 
-
-
-
-}
-    catch(IntrospectionException ex) {
-      ex.printStackTrace();
-      return null;
-    }
-  }
-  public java.awt.Image getIcon(int iconKind) {
-    switch (iconKind) {
-    case BeanInfo.ICON_COLOR_16x16:
-        return iconColor16x16Filename != null ? loadImage(iconColor16x16Filename) : null;
-    case BeanInfo.ICON_COLOR_32x32:
-        return iconColor32x32Filename != null ? loadImage(iconColor32x32Filename) : null;
-    case BeanInfo.ICON_MONO_16x16:
-        return iconMono16x16Filename != null ? loadImage(iconMono16x16Filename) : null;
-    case BeanInfo.ICON_MONO_32x32:
-        return iconMono32x32Filename != null ? loadImage(iconMono32x32Filename) : null;
-        }
-    return null;
-  }
+	@Override
+	public java.awt.Image getIcon(int iconKind) {
+		switch (iconKind) {
+		case BeanInfo.ICON_COLOR_16x16:
+			return iconColor16x16Filename != null
+					? loadImage(iconColor16x16Filename) : null;
+		case BeanInfo.ICON_COLOR_32x32:
+			return iconColor32x32Filename != null
+					? loadImage(iconColor32x32Filename) : null;
+		case BeanInfo.ICON_MONO_16x16:
+			return iconMono16x16Filename != null
+					? loadImage(iconMono16x16Filename) : null;
+		case BeanInfo.ICON_MONO_32x32:
+			return iconMono32x32Filename != null
+					? loadImage(iconMono32x32Filename) : null;
+		}
+		return null;
+	}
 }
