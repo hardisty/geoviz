7d44413bc88b31bae3a874ed14c48bfb0d0c2bff
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2008-04-11 02:28:59 +0000

    Changed to new-style header comments (just LGPL + original author info)

--- common/src/main/java/geovista/common/classification/setting/ClassifySetting2D.java
+++ common/src/main/java/geovista/common/classification/setting/ClassifySetting2D.java
@@ -1,100 +1,93 @@
-/*
- * GeoVISTA Center (Penn State, Dept. of Geography)
- * Copyright (c), 1999 - 2002, GeoVISTA Center
- * All Rights Reserved.
- *
- *
- * @author: jin Chen 
- * @date: Jan 21, 2005$
- * @version: 1.0
- */
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Jin Chen */
 package geovista.common.classification.setting;
 
 import geovista.common.classification.Range;
 
-public class ClassifySetting2D extends AbstractClassifySetting{
-    private String xVariableName, yVariableName;
-    private String xClassifier, yClassifier;
-    private int xNumOfCategory, yNumOfCategory;
-    private Range[] xBound, yBound;
+public class ClassifySetting2D extends AbstractClassifySetting {
+	private String xVariableName, yVariableName;
+	private String xClassifier, yClassifier;
+	private int xNumOfCategory, yNumOfCategory;
+	private Range[] xBound, yBound;
 
-    public ClassifySetting2D() {
-    }
+	public ClassifySetting2D() {
+	}
 
-    public ClassifySetting2D(String xVariableName, int xNumOfCategory, String xClassifier, Range[] xBound,
-                             String yVariableName, int yNumOfCategory, String yClassifier, Range[] yBound) {
-        this.xVariableName = xVariableName;
-        this.xClassifier = xClassifier;
-        this.xNumOfCategory = xNumOfCategory;
-        this.xBound = xBound;
-        this.yVariableName = yVariableName;
-        this.yClassifier = yClassifier;
-        this.yNumOfCategory = yNumOfCategory;
-        this.yBound = yBound;
-    }
+	public ClassifySetting2D(String xVariableName, int xNumOfCategory,
+			String xClassifier, Range[] xBound, String yVariableName,
+			int yNumOfCategory, String yClassifier, Range[] yBound) {
+		this.xVariableName = xVariableName;
+		this.xClassifier = xClassifier;
+		this.xNumOfCategory = xNumOfCategory;
+		this.xBound = xBound;
+		this.yVariableName = yVariableName;
+		this.yClassifier = yClassifier;
+		this.yNumOfCategory = yNumOfCategory;
+		this.yBound = yBound;
+	}
 
-    public String getxVariableName() {
-        return xVariableName;
-    }
+	public String getxVariableName() {
+		return xVariableName;
+	}
 
-    public void setxVariableName(String xVariableName) {
-        this.xVariableName = xVariableName;
-    }
-   
-    public String getxClassifier() {
-        return xClassifier;
-    }
+	public void setxVariableName(String xVariableName) {
+		this.xVariableName = xVariableName;
+	}
 
-    public void setxClassifier(String xClassifier) {
-        this.xClassifier = xClassifier;
-    }
-    
+	public String getxClassifier() {
+		return xClassifier;
+	}
 
-    public int getxNumOfCategory() {
-        return xNumOfCategory;
-    }
+	public void setxClassifier(String xClassifier) {
+		this.xClassifier = xClassifier;
+	}
 
-    public void setxNumOfCategory(int xNumOfCategory) {
-        this.xNumOfCategory = xNumOfCategory;
-    }
+	public int getxNumOfCategory() {
+		return xNumOfCategory;
+	}
 
-    public Range[] getxBound() {
-        return xBound;
-    }
+	public void setxNumOfCategory(int xNumOfCategory) {
+		this.xNumOfCategory = xNumOfCategory;
+	}
 
-    public void setxBound(Range[] xBound) {
-        this.xBound = xBound;
-    }
+	public Range[] getxBound() {
+		return xBound;
+	}
 
-    public String getyVariableName() {
-        return yVariableName;
-    }
+	public void setxBound(Range[] xBound) {
+		this.xBound = xBound;
+	}
 
-    public void setyVariableName(String yVariableName) {
-        this.yVariableName = yVariableName;
-    }
-    
-    public String getyClassifier() {
-        return yClassifier;
-    }
+	public String getyVariableName() {
+		return yVariableName;
+	}
 
-    public void setyClassifier(String yClassifier) {
-        this.yClassifier = yClassifier;
-    }
+	public void setyVariableName(String yVariableName) {
+		this.yVariableName = yVariableName;
+	}
 
-    public int getyNumOfCategory() {
-        return yNumOfCategory;
-    }
+	public String getyClassifier() {
+		return yClassifier;
+	}
 
-    public void setyNumOfCategory(int yNumOfCategory) {
-        this.yNumOfCategory = yNumOfCategory;
-    }
+	public void setyClassifier(String yClassifier) {
+		this.yClassifier = yClassifier;
+	}
 
-    public Range[] getyBound() {
-        return yBound;
-    }
+	public int getyNumOfCategory() {
+		return yNumOfCategory;
+	}
 
-    public void setyBound(Range[] yBound) {
-        this.yBound = yBound;
-    }
+	public void setyNumOfCategory(int yNumOfCategory) {
+		this.yNumOfCategory = yNumOfCategory;
+	}
+
+	public Range[] getyBound() {
+		return yBound;
+	}
+
+	public void setyBound(Range[] yBound) {
+		this.yBound = yBound;
+	}
 }
