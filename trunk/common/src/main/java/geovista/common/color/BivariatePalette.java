5605c953b2ab80f7bdcc94d7aa32464a771f3595
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2008-04-20 01:30:11 +0000

    nabbed Chris's Oblivion ColorBrewer code

--- trunk/common/src/main/java/geovista/common/color/BivariatePalette.java
+++ trunk/common/src/main/java/geovista/common/color/BivariatePalette.java
@@ -1,30 +1,26 @@
-/*
- * BivariatePalette.java
- *
- * Created on December 8, 2003, 12:20 PM
- */
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.*/
 
 package geovista.common.color;
 
 import java.awt.Color;
 
 /**
- *
- * @author  jamesm
+ * 
+ * @author jamesm
  */
 public interface BivariatePalette {
-    public static final int SEQUENTIAL_SEQUENTIAL = 1;
-    public static final int SEQUENTIAL_DIVERGING = 2;
-    public static final int SEQUENTIAL_QUALITATIVE = 3;
-    public static final int QUALITATIVE_SEQUENTIAL = 4;
-    public static final int DIVERGING_DIVERGING = 5;
-    public static final int DIVERGING_SEQUENTIAL = 6;
-    
-    
-    public String getName(); 
-    public int getType();
-    
-    public Color[][] getColors(int cols, int rows);
-    
-    
+	public static final int SEQUENTIAL_SEQUENTIAL = 1;
+	public static final int SEQUENTIAL_DIVERGING = 2;
+	public static final int SEQUENTIAL_QUALITATIVE = 3;
+	public static final int QUALITATIVE_SEQUENTIAL = 4;
+	public static final int DIVERGING_DIVERGING = 5;
+	public static final int DIVERGING_SEQUENTIAL = 6;
+
+	public String getName();
+
+	public int getType();
+
+	public Color[][] getColors(int cols, int rows);
+
 }
