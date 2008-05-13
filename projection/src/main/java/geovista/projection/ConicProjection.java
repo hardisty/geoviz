123f592c50e5d08970f0a605476b135f04175a6a
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2008-05-13 14:46:12 +0000

    fixing datasetbroadcaster to preserve data source name

--- projection/src/main/java/geovista/projection/ConicProjection.java
+++ projection/src/main/java/geovista/projection/ConicProjection.java
@@ -12,17 +12,16 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+ */
 
 package geovista.projection;
-
-import java.awt.*;
 
 /**
  * The superclass for all Conic projections.
  */
 public class ConicProjection extends Projection {
-	
+
+	@Override
 	public String toString() {
 		return "Conic";
 	}
