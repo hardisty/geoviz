03cfecebe8cd11519605b58c437679abe02a82d4
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2008-05-17 03:32:18 +0000

    gtd reading classes

---
+++ toolkitcore/src/main/java/geovista/toolkitcore/data/GeoDataGTD.java
@@ -0,0 +1,29 @@
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
+
+package geovista.toolkitcore.data;
+
+import geovista.common.data.DataSetForApps;
+import geovista.geoviz.sample.GeoDataClassResource;
+import geovista.geoviz.shapefile.ShapeFileDataReader;
+
+/**
+ * Reads shapefiles from included resources
+ * 
+ * Object[0] = names of variables 0bject[1] = data (double[], int[], or
+ * String[]) 0bject[1] = data (double[], int[], or String[]) ... Object[n-1] =
+ * the shapefile data
+ * 
+ * also see DBaseFile, ShapeFile
+ * 
+ */
+public class GeoDataGTD extends GeoDataClassResource {
+
+	@Override
+	protected DataSetForApps makeDataSetForApps() {
+		return ShapeFileDataReader.makeDataSetForAppsCsv(this.getClass(), "sc");
+
+	}
+
+}
