03d0d756d21d5488cdc65948f5f71e91843902ee
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2008-12-07 12:12:10 +0000

    looks like a formatting change to me

--- geoviz/src/main/java/geovista/geoviz/map/LayerPolygon.java
+++ geoviz/src/main/java/geovista/geoviz/map/LayerPolygon.java
@@ -20,8 +20,6 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  -------------------------------------------------------------------   */
 
-
-
 package geovista.geoviz.map;
 
 import java.awt.Rectangle;
@@ -32,86 +30,87 @@ import java.util.Vector;
 /**
  * put your documentation comment here
  */
-public class LayerPolygon
-    extends LayerShape {
+public class LayerPolygon extends LayerShape {
 
+	/*
+	 * SelectionX1 is expected to be less than selectionX2, same with Y1 and y2.
+	 * Selected observations should be rendered with the color "colorSelection".
+	 */
+	@Override
+	public void findSelection(int selectionX1, int selectionX2,
+			int selectionY1, int selectionY2) {
+		Rectangle selBox = new Rectangle(selectionX1, selectionY1, selectionX2
+				- selectionX1, selectionY2 - selectionY1);
 
-  /*
-   * SelectionX1 is expected to be less than selectionX2, same with Y1 and y2.
-   * Selected observations should be rendered with the color "colorSelection".
-   */
-  public void findSelection(int selectionX1, int selectionX2, int selectionY1,
-                            int selectionY2) {
-    Rectangle selBox = new Rectangle(selectionX1, selectionY1,
-                                     selectionX2 - selectionX1,
-                                     selectionY2 - selectionY1);
+		Vector selObs = new Vector();
+		for (int i = 0; i < spatialData.length; i++) {
+			Rectangle shpBox = spatialData[i].getBounds();
+			if (selBox.intersects(shpBox)) {
+				if (spatialData[i].contains(selBox)
+						|| spatialData[i].intersects(selBox)) {
+					selObs.add(new Integer(i));
+				} // end if really intersects
+			} // end if rough intersects
+		} // next
+		selectedObservations = new int[selObs.size()];
+		int j = 0;
+		for (Enumeration e = selObs.elements(); e.hasMoreElements();) {
+			Integer anInt = (Integer) e.nextElement();
+			selectedObservations[j] = anInt.intValue();
+			j++;
+		}
+	}
 
-    Vector selObs = new Vector();
-    for (int i = 0; i < this.spatialData.length; i++) {
-      Rectangle shpBox = this.spatialData[i].getBounds();
-      if (selBox.intersects(shpBox)) {
-        if (this.spatialData[i].contains(selBox) ||
-            this.spatialData[i].intersects(selBox)) {
-          selObs.add(new Integer(i));
-        } //end if really intersects
-      } //end if rough intersects
-    } //next
-    this.selectedObservations = new int[selObs.size()];
-    int j = 0;
-    for (Enumeration e = selObs.elements(); e.hasMoreElements(); ) {
-      Integer anInt = (Integer) e.nextElement();
-      this.selectedObservations[j] = anInt.intValue();
-      j++;
-    }
-  }
+	/*
+	 * selectionX1 is expected to be less than selectionX2, same with Y1 and y2
+	 */
+	@Override
+	public void findSelectionShift(int selectionX1, int selectionX2,
+			int selectionY1, int selectionY2) {
+		Rectangle selBox = new Rectangle(selectionX1, selectionY1, selectionX2
+				- selectionX1, selectionY2 - selectionY1);
 
-  /*
-   * selectionX1 is expected to be less than selectionX2, same with Y1 and y2
-   */
-  public void findSelectionShift(int selectionX1, int selectionX2,
-                                 int selectionY1,
-                                 int selectionY2) {
-    Rectangle selBox = new Rectangle(selectionX1, selectionY1,
-                                     selectionX2 - selectionX1,
-                                     selectionY2 - selectionY1);
+		Vector selObs = new Vector();
+		Arrays.sort(selectedObservations); // have to do this for the searching
+		for (int i = 0; i < spatialData.length; i++) {
+			Rectangle shpBox = spatialData[i].getBounds();
+			if (selBox.intersects(shpBox)) {
+				if (Arrays.binarySearch(selectedObservations, i) < 0) {
+					selObs.add(new Integer(i));
+				}
+			}
+		}
+		int[] selectedObserCp = new int[selectedObservations.length];
+		selectedObserCp = (selectedObservations.clone());
+		selectedObservations = new int[selectedObserCp.length + selObs.size()];
+		int j = 0;
+		for (j = 0; j < selectedObserCp.length; j++) {
+			selectedObservations[j] = selectedObserCp[j];
+		}
+		for (Enumeration e = selObs.elements(); e.hasMoreElements();) {
+			Integer anInt = (Integer) e.nextElement();
+			selectedObservations[j] = anInt.intValue();
+			j++;
+		}
 
-    Vector selObs = new Vector();
-    Arrays.sort(this.selectedObservations); //have to do this for the searching
-    for (int i = 0; i < this.spatialData.length; i++) {
-      Rectangle shpBox = this.spatialData[i].getBounds();
-      if (selBox.intersects(shpBox)) {
-        if (Arrays.binarySearch(this.selectedObservations, i) < 0) {
-          selObs.add(new Integer(i));
-        }
-      }
-    }
-    int[] selectedObserCp = new int[this.selectedObservations.length];
-    selectedObserCp = (int[]) (this.selectedObservations.clone());
-    this.selectedObservations = new int[selectedObserCp.length + selObs.size()];
-    int j = 0;
-    for (j = 0; j < selectedObserCp.length; j++) {
-      this.selectedObservations[j] = selectedObserCp[j];
-    }
-    for (Enumeration e = selObs.elements(); e.hasMoreElements(); ) {
-      Integer anInt = (Integer) e.nextElement();
-      this.selectedObservations[j] = anInt.intValue();
-      j++;
-    }
+	}
 
-  }
+	@Override
+	public int findIndication(int x, int y) {
+		if (spatialData == null) {
+			return Integer.MIN_VALUE;
+		}
+		for (int i = 0; i < spatialData.length; i++) {
+			Rectangle shpBox = spatialData[i].getBounds();
+			if (shpBox.contains(x, y)) {
+				if (spatialData[i].contains(x, y)) {
+					return i;
 
-  public int findIndication(int x, int y) {
-    for (int i = 0; i < this.spatialData.length; i++) {
-      Rectangle shpBox = this.spatialData[i].getBounds();
-      if (shpBox.contains(x, y)) {
-        if (this.spatialData[i].contains(x, y)) {
-          return i;
-
-        } //end if really intersects
-      } //end if rough intersects
-    } //next
-    //couldn't find anything, so
-    return Integer.MIN_VALUE;
-  }
+				} // end if really intersects
+			} // end if rough intersects
+		} // next
+		// couldn't find anything, so
+		return Integer.MIN_VALUE;
+	}
 
 }
