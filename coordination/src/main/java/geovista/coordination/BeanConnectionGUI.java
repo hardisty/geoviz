dfb4a666d7e0c3756ce6e6dc17de79b6db789cbb
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2009-05-18 15:27:43 +0000

    improved comments and removed unneeded methods

--- coordination/src/main/java/geovista/coordination/BeanConnectionGUI.java
+++ coordination/src/main/java/geovista/coordination/BeanConnectionGUI.java
@@ -1,15 +1,6 @@
-/* -------------------------------------------------------------------
- GeoVISTA Center (Penn State, Dept. of Geography)
- Java source file for the class FiringBeanGUI
- Copyright (c), 2002, GeoVISTA Center
- All Rights Reserved.
- Original Author: Frank Hardisty
- $Author: hardisty $
- $Id: BeanConnectionGUI.java,v 1.2 2003/04/25 17:58:13 hardisty Exp $
- $Date: 2003/04/25 17:58:13 $
- Reference:        Document no:
- ___                ___
- -------------------------------------------------------------------  */
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
 package geovista.coordination;
 
 import java.awt.event.ItemEvent;
@@ -20,51 +11,51 @@ import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
 /**
- *  This subclass of JPanel is responsible for communicating between the
- *  user and an instance of CoordinationManager concerning the status of
- *  one FiringBean -> ListeningBean connection for one type of coordinated
- *  event. It has a JCheckBox.
- *  
+ * This subclass of JPanel is responsible for communicating between the user and
+ * an instance of CoordinationManager concerning the status of one FiringBean ->
+ * ListeningBean connection for one type of coordinated event. It has a
+ * JCheckBox.
+ * 
  * @see CoordinationManager
  * @author Frank Hardisty
  */
 public class BeanConnectionGUI extends JPanel implements ItemListener {
-  private transient ListeningBean lBean;
-  private transient FiringMethod meth; //contains reference to parent FiringBean
-  private transient CoordinationManager cm;
-  private transient JCheckBox cBox;
+	private transient final ListeningBean lBean;
+	private transient final FiringMethod meth; // contains reference to parent
+												// FiringBean
+	private transient final CoordinationManager cm;
+	private transient final JCheckBox cBox;
 
-  /**
+	/**
    *
   */
-  public BeanConnectionGUI(ListeningBean lBean, FiringMethod meth,
-                           CoordinationManager cm) {
-    FiringBean fBean = meth.getFiringBean();
-    this.cBox = new JCheckBox();
-    //cBox.setAlignmentX(Component.LEFT_ALIGNMENT);
-    cBox.setSelected(true);
+	public BeanConnectionGUI(ListeningBean lBean, FiringMethod meth,
+			CoordinationManager cm) {
+		FiringBean fBean = meth.getFiringBean();
+		cBox = new JCheckBox();
+		// cBox.setAlignmentX(Component.LEFT_ALIGNMENT);
+		cBox.setSelected(true);
 
-    JLabel lab = new JLabel(fBean.getBeanName(), fBean.getSmallIcon(),
-                            JLabel.LEFT);
+		JLabel lab = new JLabel(fBean.getBeanName(), fBean.getSmallIcon(),
+				JLabel.LEFT);
 
-    this.setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
-    this.add(cBox);
-    this.add(lab);
-    this.lBean = lBean;
-    this.meth = meth;
-    this.cm = cm;
-    //this.setAlignmentX(Component.LEFT_ALIGNMENT);
-    cBox.addItemListener(this);
-  }
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		this.add(cBox);
+		this.add(lab);
+		this.lBean = lBean;
+		this.meth = meth;
+		this.cm = cm;
+		// this.setAlignmentX(Component.LEFT_ALIGNMENT);
+		cBox.addItemListener(this);
+	}
 
-  /** We listen to our box*/
-  public void itemStateChanged(ItemEvent e) {
-    if (e.getStateChange() == ItemEvent.DESELECTED) {
-      cm.disconnectBeans(this.meth,this.lBean);
-    } else if (e.getStateChange() == ItemEvent.SELECTED) {
-      cm.reconnectBeans(this.meth,this.lBean);
-    }
-  }
+	/** We listen to our box */
+	public void itemStateChanged(ItemEvent e) {
+		if (e.getStateChange() == ItemEvent.DESELECTED) {
+			cm.disconnectBeans(meth, lBean);
+		} else if (e.getStateChange() == ItemEvent.SELECTED) {
+			cm.reconnectBeans(meth, lBean);
+		}
+	}
 }
--- coordination/src/main/java/geovista/coordination/BeanShortcutsGUI.java
+++ coordination/src/main/java/geovista/coordination/BeanShortcutsGUI.java
@@ -1,16 +1,6 @@
-/* -------------------------------------------------------------------
- GeoVISTA Center (Penn State, Dept. of Geography)
- Java source file for the class BeanShortcutsGUI
- Copyright (c), 2002, GeoVISTA Center
- All Rights Reserved.
- Original Author: Frank Hardisty
- $Author: hardisty $
- $Id: BeanShortcutsGUI.java,v 1.2 2003/04/25 17:58:13 hardisty Exp $
- $Date: 2003/04/25 17:58:13 $
- Reference:        Document no:
- ___                ___
- -------------------------------------------------------------------  *
- */
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
 package geovista.coordination;
 
 import java.awt.Color;
@@ -27,128 +17,132 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.event.EventListenerList;
 
-
 /**
  * 
- *  This class provides shortcut buttons to connect or disconnect all of the
- *  incoming or outgoing coordinated connections for this bean. The original
- *  design calls for a CoordinationManagerGUI to hold a reference to one of
- *  these per coordinated bean, and to listen for GUI events.
+ * This class provides shortcut buttons to connect or disconnect all of the
+ * incoming or outgoing coordinated connections for this bean. The original
+ * design calls for a CoordinationManagerGUI to hold a reference to one of these
+ * per coordinated bean, and to listen for GUI events.
  * 
- *  
+ * 
  * @see CoordinationManager
  * @author Frank Hardisty
  */
 public class BeanShortcutsGUI extends JPanel implements ActionListener {
-  static public String EVENT_INCOMING_ON = "incomingOn";
-  static public String EVENT_INCOMING_OFF = "incomingOff";
-  static public String EVENT_OUTGOING_ON = "outgoingOn";
-  static public String EVENT_OUTGOING_OFF = "outgoingOff";
-  private transient JButton incomingOn;
-  private transient JButton incomingOff;
-  private transient JButton outgoingOn;
-  private transient JButton outgoingOff;
-  private transient JPanel buttonsPanel;
+	static public String EVENT_INCOMING_ON = "incomingOn";
+	static public String EVENT_INCOMING_OFF = "incomingOff";
+	static public String EVENT_OUTGOING_ON = "outgoingOn";
+	static public String EVENT_OUTGOING_OFF = "outgoingOff";
+	private transient final JButton incomingOn;
+	private transient final JButton incomingOff;
+	private transient final JButton outgoingOn;
+	private transient final JButton outgoingOff;
+	private transient final JPanel buttonsPanel;
 
-  /**
+	/**
    *
   */
-  public BeanShortcutsGUI(String beanName, Icon icon) {
-    this.setBorder(BorderFactory.createLineBorder(Color.blue));
-    this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
-    JLabel preTitle = new JLabel("Shortcuts for:");
-    preTitle.setAlignmentX(Component.LEFT_ALIGNMENT);
-    this.add(preTitle);
-    JLabel title = new JLabel(beanName, icon, JLabel.CENTER);
-    title.setAlignmentX(Component.LEFT_ALIGNMENT);
-    this.add(title);
+	public BeanShortcutsGUI(String beanName, Icon icon) {
+		setBorder(BorderFactory.createLineBorder(Color.blue));
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		JLabel preTitle = new JLabel("Shortcuts for:");
+		preTitle.setAlignmentX(Component.LEFT_ALIGNMENT);
+		this.add(preTitle);
+		JLabel title = new JLabel(beanName, icon, JLabel.CENTER);
+		title.setAlignmentX(Component.LEFT_ALIGNMENT);
+		this.add(title);
 
-    this.incomingOn = new JButton("Incoming On");
-    this.incomingOff = new JButton("Incoming Off");
-    this.outgoingOn = new JButton("Outgoing On");
-    this.outgoingOff = new JButton("Outgoing Off");
+		incomingOn = new JButton("Incoming On");
+		incomingOff = new JButton("Incoming Off");
+		outgoingOn = new JButton("Outgoing On");
+		outgoingOff = new JButton("Outgoing Off");
 
-    this.incomingOn.addActionListener(this);
-    this.incomingOff.addActionListener(this);
-    this.outgoingOn.addActionListener(this);
-    this.outgoingOff.addActionListener(this);
+		incomingOn.addActionListener(this);
+		incomingOff.addActionListener(this);
+		outgoingOn.addActionListener(this);
+		outgoingOff.addActionListener(this);
 
-    Font f = new Font("", Font.PLAIN, 10); //decreasing the size to 10 point
+		Font f = new Font("", Font.PLAIN, 10); // decreasing the size to 10
+												// point
 
-    this.incomingOn.setFont(f);
-    this.incomingOff.setFont(f);
-    this.outgoingOn.setFont(f);
-    this.outgoingOff.setFont(f);
+		incomingOn.setFont(f);
+		incomingOff.setFont(f);
+		outgoingOn.setFont(f);
+		outgoingOff.setFont(f);
 
-    buttonsPanel = new JPanel();
-    buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
+		buttonsPanel = new JPanel();
+		buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
 
-    buttonsPanel.add(incomingOn);
-    buttonsPanel.add(incomingOff);
-    buttonsPanel.add(outgoingOn);
-    buttonsPanel.add(outgoingOff);
-    this.add(buttonsPanel);
-  }
+		buttonsPanel.add(incomingOn);
+		buttonsPanel.add(incomingOff);
+		buttonsPanel.add(outgoingOn);
+		buttonsPanel.add(outgoingOff);
+		this.add(buttonsPanel);
+	}
 
-  /** Listens to the buttons*/
-  public void actionPerformed(ActionEvent e) {
-    Object source = e.getSource();
+	/** Listens to the buttons */
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
 
-    if (source instanceof JButton) {
-      JButton but = (JButton) e.getSource();
-      String butName = but.getText();
+		if (source instanceof JButton) {
+			JButton but = (JButton) e.getSource();
+			String butName = but.getText();
 
-      if (butName == this.incomingOn.getText()) {
-        this.fireActionPerformed(BeanShortcutsGUI.EVENT_INCOMING_ON);
-      }
+			if (butName == incomingOn.getText()) {
+				fireActionPerformed(BeanShortcutsGUI.EVENT_INCOMING_ON);
+			}
 
-      if (butName == this.incomingOff.getText()) {
-        this.fireActionPerformed(BeanShortcutsGUI.EVENT_INCOMING_OFF);
-      }
+			if (butName == incomingOff.getText()) {
+				fireActionPerformed(BeanShortcutsGUI.EVENT_INCOMING_OFF);
+			}
 
-      if (butName == this.outgoingOn.getText()) {
-        this.fireActionPerformed(BeanShortcutsGUI.EVENT_OUTGOING_ON);
-      }
+			if (butName == outgoingOn.getText()) {
+				fireActionPerformed(BeanShortcutsGUI.EVENT_OUTGOING_ON);
+			}
 
-      if (butName == this.outgoingOff.getText()) {
-        this.fireActionPerformed(BeanShortcutsGUI.EVENT_OUTGOING_OFF);
-      }
-    } // end if
-  }
-  /**
-   * adds an ActionListener, e.g. CoordinationManagerGUI
-   */
-  public void addActionListener(ActionListener l) {
-      listenerList.add(ActionListener.class, l);
-  }
+			if (butName == outgoingOff.getText()) {
+				fireActionPerformed(BeanShortcutsGUI.EVENT_OUTGOING_OFF);
+			}
+		} // end if
+	}
 
-  /**
-   * removes an ActionListener, e.g. CoordinationManagerGUI
-   */
-  public void removeActionListener(ActionListener l) {
-      listenerList.remove(ActionListener.class, l);
-    }
-  /**
-  * Notify all listeners that have registered interest for
-  * notification on this event type.
-  * @see EventListenerList
-  */
-  protected void fireActionPerformed(String command) {
-    // Guaranteed to return a non-null array
-    Object[] listeners = listenerList.getListenerList();
-    ActionEvent e = null;
+	/**
+	 * adds an ActionListener, e.g. CoordinationManagerGUI
+	 */
+	public void addActionListener(ActionListener l) {
+		listenerList.add(ActionListener.class, l);
+	}
 
-    // Process the listeners last to first, notifying
-    // those that are interested in this event
-    for (int i = listeners.length - 2; i >= 0; i -= 2) {
-      if (listeners[i] == ActionListener.class) {
-        // Lazily create the event:
-        if (e == null) {
-          e = new ActionEvent(this, ActionEvent.ACTION_PERFORMED, command);
-        }
+	/**
+	 * removes an ActionListener, e.g. CoordinationManagerGUI
+	 */
+	public void removeActionListener(ActionListener l) {
+		listenerList.remove(ActionListener.class, l);
+	}
 
-        ((ActionListener) listeners[i + 1]).actionPerformed(e);
-      }
-    } //next listener
-  } //end method
+	/**
+	 * Notify all listeners that have registered interest for notification on
+	 * this event type.
+	 * 
+	 * @see EventListenerList
+	 */
+	protected void fireActionPerformed(String command) {
+		// Guaranteed to return a non-null array
+		Object[] listeners = listenerList.getListenerList();
+		ActionEvent e = null;
+
+		// Process the listeners last to first, notifying
+		// those that are interested in this event
+		for (int i = listeners.length - 2; i >= 0; i -= 2) {
+			if (listeners[i] == ActionListener.class) {
+				// Lazily create the event:
+				if (e == null) {
+					e = new ActionEvent(this, ActionEvent.ACTION_PERFORMED,
+							command);
+				}
+
+				((ActionListener) listeners[i + 1]).actionPerformed(e);
+			}
+		} // next listener
+	} // end method
 }
