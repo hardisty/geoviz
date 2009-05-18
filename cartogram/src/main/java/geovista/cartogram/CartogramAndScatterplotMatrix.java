1236eb6b0d613b05753d4474770c33d166210c5f
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2009-05-18 15:12:42 +0000

    fixed licensing

--- cartogram/src/main/java/geovista/cartogram/CartogramAndScatterplotMatrix.java
+++ cartogram/src/main/java/geovista/cartogram/CartogramAndScatterplotMatrix.java
@@ -1,44 +1,20 @@
-/* -------------------------------------------------------------------
- GeoVISTA Center (Penn State, Dept. of Geography)
- Java source file for the class CartogramAndScatterplotMatrix
- Original Author: Frank Hardisty
- $Author: hardisty $
- $Id: CartogramAndScatterplotMatrix.java,v 1.1 2005/03/19 00:48:39 hardisty Exp $
- $Date: 2005/03/19 00:48:39 $
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- -------------------------------------------------------------------   */
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
 package geovista.cartogram;
 
 import geovista.geoviz.scatterplot.ScatterPlot;
 import geovista.matrix.BiPlotMatrix;
 
-/**
- *
- * @author Frank Hardisty
- * 
- */
-public class CartogramAndScatterplotMatrix
-    extends BiPlotMatrix
+public class CartogramAndScatterplotMatrix extends BiPlotMatrix
 
 {
 
-  public CartogramAndScatterplotMatrix() {
-    super();
-    this.setElementClass1(ScatterPlot.class);
-    this.setElementClass2(CartogramMatrixElement.class);
+	public CartogramAndScatterplotMatrix() {
+		super();
+		this.setElementClass1(ScatterPlot.class);
+		this.setElementClass2(CartogramMatrixElement.class);
 
-  }
+	}
 
-
-
-} //end class
+}
