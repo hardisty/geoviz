00b2959623df160cfb148821582f3eb0b3cf7e1a
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2009-05-21 15:02:04 +0000

    fixed licensing, improved header comments

--- trunk/rjava/src/main/java/geovista/rjava/DataSetModifiedListener.java
+++ trunk/rjava/src/main/java/geovista/rjava/DataSetModifiedListener.java
@@ -1,26 +1,6 @@
-/* -------------------------------------------------------------------
- GeoVISTA Center (Penn State, Dept. of Geography)
- Java source file for the class ColumnAppendedListener
- Copyright (c), 2002, GeoVISTA Center
- All Rights Reserved.
- Original Author: Frank Hardisty
- $Author: hardisty $
- $Id: ColumnAppendedListener.java,v 1.1 2005/02/13 03:26:27 hardisty Exp $
- $Date: 2005/02/13 03:26:27 $
- This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-  -------------------------------------------------------------------   */
-
-
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
 
 package geovista.rjava;
 
@@ -28,18 +8,15 @@ import java.util.EventListener;
 
 import geovista.common.event.ColumnAppendedEvent;
 
-
 /**
  * This interface enables listening to senders of DataSetEvents.
- *
- * This interface also enables "fireDataSetChanged" methods in classes
- * that generate and broadcast DataSetEvents.
- *
+ * 
+ * This interface also enables "fireDataSetChanged" methods in classes that
+ * generate and broadcast DataSetEvents.
+ * 
  */
 public interface DataSetModifiedListener extends EventListener {
 
-
-  public void dataSetModified(ColumnAppendedEvent e);
-
+	public void dataSetModified(ColumnAppendedEvent e);
 
 }
