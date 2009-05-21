0ef560de26bc68a70d750abaabd7a1aa304a6ef6
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2009-05-21 13:10:21 +0000

    fixed licensing, improved header comments

--- geoviz/src/main/java/geovista/geoviz/spreadsheet/TableViewerBeanInfo.java
+++ geoviz/src/main/java/geovista/geoviz/spreadsheet/TableViewerBeanInfo.java
@@ -1,24 +1,6 @@
-/* -------------------------------------------------------------------
- GeoVISTA Center (Penn State, Dept. of Geography)
- Java source file for the class GeoMapBeanInfo
- Copyright (c), 2002, GeoVISTA Center
- All Rights Reserved.
- Original Author: Frank Hardisty
- $Author: hardisty $
- $Id: GeoMapBeanInfo.java,v 1.2 2004/05/05 17:23:10 hardisty Exp $
- $Date: 2004/05/05 17:23:10 $
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- -------------------------------------------------------------------   */
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
 package geovista.geoviz.spreadsheet;
 
 import java.beans.BeanInfo;
@@ -27,49 +9,50 @@ import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.beans.SimpleBeanInfo;
 
+public class TableViewerBeanInfo extends SimpleBeanInfo {
+	static Class beanClass = TableViewer.class;
+	static String iconColor16x16Filename = "resources/spreadsheet16.gif";
+	static String iconColor32x32Filename = "resources/spreadsheet32.gif";
+	static String iconMono16x16Filename;
+	static String iconMono32x32Filename;
 
-public class TableViewerBeanInfo
-    extends SimpleBeanInfo {
-  static Class beanClass = TableViewer.class;
-  static String iconColor16x16Filename = "resources/spreadsheet16.gif";
-  static String iconColor32x32Filename = "resources/spreadsheet32.gif";
-  static String iconMono16x16Filename;
-  static String iconMono32x32Filename;
+	public TableViewerBeanInfo() {
+	}
 
-  public TableViewerBeanInfo() {
-  }
+	@Override
+	public PropertyDescriptor[] getPropertyDescriptors() {
+		PropertyDescriptor[] pds = new PropertyDescriptor[] {};
+		return pds;
+	}
 
-  public PropertyDescriptor[] getPropertyDescriptors() {
-    PropertyDescriptor[] pds = new PropertyDescriptor[] {};
-    return pds;
-  }
+	@Override
+	public java.awt.Image getIcon(int iconKind) {
+		switch (iconKind) {
+		case BeanInfo.ICON_COLOR_16x16:
+			return iconColor16x16Filename != null
+					? loadImage(iconColor16x16Filename) : null;
+		case BeanInfo.ICON_COLOR_32x32:
+			return iconColor32x32Filename != null
+					? loadImage(iconColor32x32Filename) : null;
+		case BeanInfo.ICON_MONO_16x16:
+			return iconMono16x16Filename != null
+					? loadImage(iconMono16x16Filename) : null;
+		case BeanInfo.ICON_MONO_32x32:
+			return iconMono32x32Filename != null
+					? loadImage(iconMono32x32Filename) : null;
+		}
+		return null;
+	}
 
-  public java.awt.Image getIcon(int iconKind) {
-    switch (iconKind) {
-      case BeanInfo.ICON_COLOR_16x16:
-        return iconColor16x16Filename != null ?
-            loadImage(iconColor16x16Filename) : null;
-      case BeanInfo.ICON_COLOR_32x32:
-        return iconColor32x32Filename != null ?
-            loadImage(iconColor32x32Filename) : null;
-      case BeanInfo.ICON_MONO_16x16:
-        return iconMono16x16Filename != null ? loadImage(iconMono16x16Filename) : null;
-      case BeanInfo.ICON_MONO_32x32:
-        return iconMono32x32Filename != null ? loadImage(iconMono32x32Filename) : null;
-    }
-    return null;
-  }
-
-  public BeanInfo[] getAdditionalBeanInfo() {
-    Class superclass = beanClass.getSuperclass();
-    try {
-      BeanInfo superBeanInfo = Introspector.getBeanInfo(superclass);
-      return new BeanInfo[] {
-          superBeanInfo};
-    }
-    catch (IntrospectionException ex) {
-      ex.printStackTrace();
-      return null;
-    }
-  }
+	@Override
+	public BeanInfo[] getAdditionalBeanInfo() {
+		Class superclass = beanClass.getSuperclass();
+		try {
+			BeanInfo superBeanInfo = Introspector.getBeanInfo(superclass);
+			return new BeanInfo[] { superBeanInfo };
+		} catch (IntrospectionException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
 }
