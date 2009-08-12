31c096d9133da00edbc12cab7c365698669db663
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2009-08-12 01:43:33 +0000

    unit tests for GeoViz Toolkit Components

--- trunk/sound/src/test/java/geovista/sound/SonicClassifierTest.java
+++ trunk/sound/src/test/java/geovista/sound/SonicClassifierTest.java
@@ -4,38 +4,16 @@
 
 package geovista.sound;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-import geovista.coordination.CoordinationManager;
-import geovista.readers.example.GeoDataGeneralizedStates;
-import geovista.readers.shapefile.ShapeFileToShape;
+import geovista.geoviz.Exerciser;
 
 public class SonicClassifierTest extends TestCase {
-	protected SonicClassifier sClasser;
+	public void testSonicClassifier() {
 
-	public SonicClassifierTest(String name) {
-		super(name);
-		setUp();
-	}
-
-	@Override
-	protected void setUp() {
-		// sClasser = new SonicClassifier();
-		CoordinationManager coord = new CoordinationManager();
-		GeoDataGeneralizedStates stateData = new GeoDataGeneralizedStates();
-		ShapeFileToShape shpToShape = new ShapeFileToShape();
-		// coord.addBean(sClasser);
-		coord.addBean(shpToShape);
-		shpToShape.setInputDataSet(stateData.getDataSet());
-	}
-
-	public static Test suite() {
-		return new TestSuite(SonicClassifierTest.class);
-	}
-
-	public void testGetRampedValueRGB() {
+		SonicClassifier comp = new SonicClassifier();
+		Exerciser exer = new Exerciser();
+		exer.testGUIAndEvents(comp);
 
 	}
 
