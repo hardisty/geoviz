f6944a13d01c4f7da345a9a5bfe079f0223c42cd
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2009-08-12 01:28:29 +0000

    improved PolygonShape getBounds method

--- common/src/main/java/geovista/common/jts/PolygonShape.java
+++ common/src/main/java/geovista/common/jts/PolygonShape.java
@@ -1,4 +1,3 @@
-
 /*
  * The Unified Mapping Platform (JUMP) is an extensible, interactive GUI 
  * for visualizing and manipulating spatial features with geometry and attributes.
@@ -32,7 +31,7 @@
  */
 
 package geovista.common.jts;
- 
+
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
@@ -46,96 +45,96 @@ import java.util.Iterator;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
-
 public class PolygonShape implements Shape {
-    private GeneralPath shell;
-    private ArrayList holes = new ArrayList();
+	private final GeneralPath shell;
+	private final ArrayList holes = new ArrayList();
 
-    /**
-     * @param shellVertices in view coordinates
-     * @param holeVerticesCollection a Coordinate[] for each hole, in view coordinates
-     */
-    public PolygonShape(Coordinate[] shellVertices,
-        Collection holeVerticesCollection) {
-        shell = toPolygon(shellVertices);
+	/**
+	 * @param shellVertices
+	 *            in view coordinates
+	 * @param holeVerticesCollection
+	 *            a Coordinate[] for each hole, in view coordinates
+	 */
+	public PolygonShape(Coordinate[] shellVertices,
+			Collection holeVerticesCollection) {
+		shell = toPolygon(shellVertices);
 
-        for (Iterator i = holeVerticesCollection.iterator(); i.hasNext();) {
-            Coordinate[] holeVertices = (Coordinate[]) i.next();
-            holes.add(toPolygon(holeVertices));
-        }
-    }
+		for (Iterator i = holeVerticesCollection.iterator(); i.hasNext();) {
+			Coordinate[] holeVertices = (Coordinate[]) i.next();
+			holes.add(toPolygon(holeVertices));
+		}
+	}
 
-    private GeneralPath toPolygon(Coordinate[] coordinates) {
-        GeneralPath polygon = new GeneralPath();
-        if (coordinates.length == 0){
-        	return polygon;
-        }
-        polygon.moveTo((float)coordinates[0].x, (float)coordinates[0].y);
-        for (int i = 0; i < coordinates.length; i++) {
-       
-            polygon.lineTo((float)coordinates[i].x, (float)coordinates[i].y);
-        }
+	private GeneralPath toPolygon(Coordinate[] coordinates) {
+		GeneralPath polygon = new GeneralPath();
+		if (coordinates.length == 0) {
+			return polygon;
+		}
+		polygon.moveTo((float) coordinates[0].x, (float) coordinates[0].y);
+		for (Coordinate coordinate : coordinates) {
 
-        return polygon;
-    }
+			polygon.lineTo((float) coordinate.x, (float) coordinate.y);
+		}
 
-    public Rectangle getBounds() {
-        /**@todo Implement this java.awt.Shape method*/
-        throw new java.lang.UnsupportedOperationException(
-            "Method getBounds() not yet implemented.");
-    }
+		return polygon;
+	}
 
-    public Rectangle2D getBounds2D() {
-        return shell.getBounds2D();
-    }
+	public Rectangle getBounds() {
+		return shell.getBounds();
 
-    public boolean contains(double x, double y) {
-        /**@todo Implement this java.awt.Shape method*/
-        throw new java.lang.UnsupportedOperationException(
-            "Method contains() not yet implemented.");
-    }
+	}
 
-    public boolean contains(Point2D p) {
-        /**@todo Implement this java.awt.Shape method*/
-        throw new java.lang.UnsupportedOperationException(
-            "Method contains() not yet implemented.");
-    }
+	public Rectangle2D getBounds2D() {
+		return shell.getBounds2D();
+	}
 
-    public boolean intersects(double x, double y, double w, double h) {
-        /**@todo Implement this java.awt.Shape method*/
-        throw new java.lang.UnsupportedOperationException(
-            "Method intersects() not yet implemented.");
-    }
+	public boolean contains(double x, double y) {
+		/** @todo Implement this java.awt.Shape method */
+		throw new java.lang.UnsupportedOperationException(
+				"Method contains() not yet implemented.");
+	}
 
-    public boolean intersects(Rectangle2D r) {
-        /**@todo Implement this java.awt.Shape method*/
-        throw new java.lang.UnsupportedOperationException(
-            "Method intersects() not yet implemented.");
-    }
+	public boolean contains(Point2D p) {
+		/** @todo Implement this java.awt.Shape method */
+		throw new java.lang.UnsupportedOperationException(
+				"Method contains() not yet implemented.");
+	}
 
-    public boolean contains(double x, double y, double w, double h) {
-        /**@todo Implement this java.awt.Shape method*/
-        throw new java.lang.UnsupportedOperationException(
-            "Method contains() not yet implemented.");
-    }
+	public boolean intersects(double x, double y, double w, double h) {
+		/** @todo Implement this java.awt.Shape method */
+		throw new java.lang.UnsupportedOperationException(
+				"Method intersects() not yet implemented.");
+	}
 
-    public boolean contains(Rectangle2D r) {
-        /**@todo Implement this java.awt.Shape method*/
-        throw new java.lang.UnsupportedOperationException(
-            "Method contains() not yet implemented.");
-    }
+	public boolean intersects(Rectangle2D r) {
+		/** @todo Implement this java.awt.Shape method */
+		throw new java.lang.UnsupportedOperationException(
+				"Method intersects() not yet implemented.");
+	}
 
-    public PathIterator getPathIterator(AffineTransform at) {
-        ArrayList rings = new ArrayList();
-        rings.add(shell);
-        rings.addAll(holes);
+	public boolean contains(double x, double y, double w, double h) {
+		/** @todo Implement this java.awt.Shape method */
+		throw new java.lang.UnsupportedOperationException(
+				"Method contains() not yet implemented.");
+	}
 
-        return new ShapeCollectionPathIterator(rings, at);
-    }
+	public boolean contains(Rectangle2D r) {
+		/** @todo Implement this java.awt.Shape method */
+		throw new java.lang.UnsupportedOperationException(
+				"Method contains() not yet implemented.");
+	}
 
-    public PathIterator getPathIterator(AffineTransform at, double flatness) {
-        // since we don't support curved geometries, can simply delegate to the simple method
-        return getPathIterator(at);
-    }
+	public PathIterator getPathIterator(AffineTransform at) {
+		ArrayList rings = new ArrayList();
+		rings.add(shell);
+		rings.addAll(holes);
+
+		return new ShapeCollectionPathIterator(rings, at);
+	}
+
+	public PathIterator getPathIterator(AffineTransform at, double flatness) {
+		// since we don't support curved geometries, can simply delegate to the
+		// simple method
+		return getPathIterator(at);
+	}
 }
-
