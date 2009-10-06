3645bcbebbb4ab62c18ed47c82fbac39bbf678f6
frank.hardisty <frank.hardisty@9eb86e29-fa2f-0410-8635-6322a288db1e>
2009-10-06 15:23:17 +0000

    a test class, how nice

---
+++ trunk/cartogram/src/test/java/geovista/cartogram/GeoMapCartogramTest.java
@@ -0,0 +1,19 @@
+/* Licensed under LGPL v. 2.1 or any later version;
+ see GNU LGPL for details.
+ Original Author: Frank Hardisty */
+
+package geovista.cartogram;
+
+import junit.framework.TestCase;
+
+import geovista.geoviz.Exerciser;
+
+public class GeoMapCartogramTest extends TestCase {
+	public void testGeoMapCartogram() {
+
+		GeoMapCartogram comp = new GeoMapCartogram();
+		Exerciser exer = new Exerciser();
+		// exer.testGUIAndEvents(comp);
+
+	}
+}
