5ed103927eed249c752171b4822846db5c506bfb
frank.hardisty@gmail.com <frank.hardisty@gmail.com@9eb86e29-fa2f-0410-8635-6322a288db1e>
2010-08-27 16:36:04 +0000

    switched NullShape to EmptyShape

--- trunk/common/src/main/java/geovista/common/jts/EmptyShape.java
+++ trunk/common/src/main/java/geovista/common/jts/EmptyShape.java
@@ -11,62 +11,95 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
 /**
- * An shape to stand in for null.
+ * An empty shape with no geometry.
  */
-public class NullShape implements Shape {
-	public final static NullShape INSTANCE = new NullShape();
+public class EmptyShape implements Shape {
+	public final static EmptyShape INSTANCE = new EmptyShape();
 	private static EmptyIterator ei = new EmptyIterator();
 	private static Rectangle er = new Rectangle(0, 0, 0, 0);
 
-	private NullShape() {
+	/**
+	 * Hides default constructor.
+	 */
+	private EmptyShape() {
 
 	}
 
+	/**
+	 * Always returns false.
+	 */
 	public boolean contains(Point2D arg0) {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
+	/**
+	 * Always returns false.
+	 */
 	public boolean contains(Rectangle2D arg0) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Always returns false.
+	 */
 	public boolean contains(double arg0, double arg1) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Always returns false.
+	 */
 	public boolean contains(double arg0, double arg1, double arg2, double arg3) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Returns an empty rectangle.
+	 */
 	public Rectangle getBounds() {
 
 		return er;
 	}
 
+	/**
+	 * Returns null;
+	 */
 	public Rectangle2D getBounds2D() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Returns an empty AffineTransform.
+	 */
 	public PathIterator getPathIterator(AffineTransform arg0) {
 		// TODO Auto-generated method stub
 		return ei;
 	}
 
+	/**
+	 * Returns an empty PathIterator.
+	 */
 	public PathIterator getPathIterator(AffineTransform arg0, double arg1) {
 		// TODO Auto-generated method stub
 		return ei;
 	}
 
+	/**
+	 * Always returns false.
+	 */
 	public boolean intersects(Rectangle2D arg0) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Always returns false.
+	 */
 	public boolean intersects(double arg0, double arg1, double arg2, double arg3) {
 		// TODO Auto-generated method stub
 		return false;
