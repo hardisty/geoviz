6e1ba89356c65294de465e09cc1ee3572af9d02f
peter.foley@gmail.com <peter.foley@gmail.com@9eb86e29-fa2f-0410-8635-6322a288db1e>
2010-08-31 18:36:02 +0000

    Latest Discriminant Analysis java bean changes

---
+++ trunk/statistics/src/main/java/ncg/statistics/DiscriminantAnalysisException.java
@@ -0,0 +1,27 @@
+package ncg.statistics;
+
+/*
+ * Exception thrown by the DiscriminantAnalysis class
+ */
+public class DiscriminantAnalysisException extends Exception {
+
+	// constructor signatures all match constructors of the Exception class
+	
+	public DiscriminantAnalysisException() {
+		super();
+	}
+	
+	public DiscriminantAnalysisException(String message) {
+		super(message);
+	}
+	
+	public DiscriminantAnalysisException(String message, Throwable cause) {
+		super(message,cause);
+	}
+	
+	public DiscriminantAnalysisException(Throwable cause) {
+		super(cause);
+	}
+	
+	
+}
--- trunk/statistics/src/main/java/ncg/statistics/DiscriminantAnalysisGUI.java
+++ trunk/statistics/src/main/java/ncg/statistics/DiscriminantAnalysisGUI.java
@@ -25,8 +25,11 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JOptionPane;
 
 import geovista.common.data.DataSetBroadcaster;
 import geovista.common.data.DataSetForApps;
@@ -37,14 +40,20 @@ import geovista.common.event.SubspaceListener;
 import geovista.common.ui.VariablePicker;
 import geovista.geoviz.map.GeoMap;
 import geovista.readers.shapefile.ShapeFileDataReader;
+import geovista.common.event.IndicationEvent;
+import geovista.common.event.IndicationListener;
+import geovista.common.event.SelectionEvent;
+import geovista.common.event.SelectionListener;
 
 /**
  * @author pfoley
  * 
  */
 @SuppressWarnings("serial")
-public class DiscriminantAnalysisGUI extends JPanel implements ActionListener,
-		DataSetListener, SubspaceListener {
+
+public class DiscriminantAnalysisGUI extends JPanel
+	implements ActionListener ,DataSetListener, SubspaceListener, SelectionListener, IndicationListener {
+	 
 
 	// backend components
 	DiscriminantAnalysis da = null;
@@ -62,43 +71,44 @@ public class DiscriminantAnalysisGUI extends JPanel implements ActionListener,
 	// in the dataSet object
 	int[] indVarIndices = new int[0];
 	int categoryIndex = -1;
-
+	int numClassifications = 0;
+	
 	// categoryIndexMap maps the indices of variables
 	// in the categoryCombo Box to indices of variables
 	// in dataSet
-	Map<Integer, Integer> categoryIndexMap = null;
-
+	Map<Integer,Integer> categoryIndexMap = null;
+	
 	// logger object
 	protected final static Logger logger = Logger
 			.getLogger(DiscriminantAnalysisGUI.class.getPackage().getName());
+
 
 	public DiscriminantAnalysisGUI() {
 
 		super(new BorderLayout());
 
-		setPreferredSize(new Dimension(800, 600));
+		setPreferredSize(new Dimension(600, 400));
 
 		// create the class variables
 		da = new DiscriminantAnalysis();
 		goButton = new JButton("Classify");
 		category = new JLabel("Category");
 		categoryCombo = new JComboBox();
-		indVarPicker = new VariablePicker();
+		indVarPicker = new VariablePicker(DataSetForApps.TYPE_DOUBLE);
 		outputInfo = new JPanel();
 		outputInfo.setLayout(new BoxLayout(outputInfo, BoxLayout.Y_AXIS));
+		JScrollPane outputInfoPane = new JScrollPane(outputInfo);
 		map = new GeoMap();
 
 		// set class variable properties
 		indVarPicker.setPreferredSize(new Dimension(180, 400));
 		indVarPicker.setBorder(BorderFactory
 				.createTitledBorder("Independent Variables"));
-		// textArea.setEditable(false);
 
 		// add items to the tabbedPane
 		JTabbedPane tabbedPane = new JTabbedPane();
 		tabbedPane.addTab("GeoMap", map);
-		tabbedPane.addTab("Diagnostics", outputInfo);
-		// tabbedPane.addTab("Info", textArea);
+		tabbedPane.addTab("Diagnostics", outputInfoPane);
 
 		// add items to the bottom panel
 		JPanel bottomPanel = new JPanel();
@@ -111,6 +121,7 @@ public class DiscriminantAnalysisGUI extends JPanel implements ActionListener,
 		this.add(indVarPicker, BorderLayout.WEST);
 		this.add(bottomPanel, BorderLayout.SOUTH);
 
+		// add this bean as a listener for the following events types:
 		goButton.addActionListener(this);
 		categoryCombo.addActionListener(this);
 		indVarPicker.addSubspaceListener(this);
@@ -151,14 +162,15 @@ public class DiscriminantAnalysisGUI extends JPanel implements ActionListener,
 		app.add(daGui);
 		app.pack();
 		app.setVisible(true);
-
-		// read the data in the test file
-		// URL testFileName =
-		// daGui.getClass().getResource("resources/electoral_divisions_cu_cso_2006.shp");
+		
+		// test file name
 		URL testFileName = daGui.getClass().getResource(
 				"resources/iris_poly.shp");
+
 		ShapeFileDataReader shpRead = new ShapeFileDataReader();
 		shpRead.setFileName(testFileName.getFile());
+		
+		// now broadcast the dataset we have just read in - this bean is listening for it (dataSetChanged method)
 		Object[] testDataArray = shpRead.getDataSet();
 
 		if (testDataArray != null) {
@@ -174,7 +186,11 @@ public class DiscriminantAnalysisGUI extends JPanel implements ActionListener,
 		}
 
 	}
-
+	
+	/*
+	 * Called when the user presses the classifier button or when a new category is chosen
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
 	public void actionPerformed(ActionEvent e) {
 
 		if (e.getSource() == categoryCombo) {
@@ -184,7 +200,11 @@ public class DiscriminantAnalysisGUI extends JPanel implements ActionListener,
 		}
 
 	}
-
+	
+	/*
+	 * Called whenever a new DataSetEvent object is broadcast
+	 * @see geovista.common.event.DataSetListener#dataSetChanged(geovista.common.event.DataSetEvent)
+	 */
 	public void dataSetChanged(DataSetEvent e) {
 
 		// get the dataset
@@ -209,9 +229,13 @@ public class DiscriminantAnalysisGUI extends JPanel implements ActionListener,
 		// use categoryCombo index for indices of items added
 		// to categoryCombo Box
 		int categoryComboIndex = -1;
-
-		for (int i = 0; i < numVars; i++) {
-
+		
+		// ensure that the categoryCombo doesn't have any existing items
+		categoryCombo.removeAllItems();
+		
+		// only include variables of type integer in the category combo box
+		for (int i = 0;  i < numVars; i++){
+						
 			if (dTypes[i] == DataSetForApps.TYPE_INTEGER) {
 
 				categoryCombo.addItem(dataSet.getColumnName(i));
@@ -224,10 +248,15 @@ public class DiscriminantAnalysisGUI extends JPanel implements ActionListener,
 		}
 
 		// the variable picker will only include variables of types
-		// int, double or boolean
+		// int, double or boolean by default. Want it to only include variables
+		// of type double so need to create a new DataSetEvent with variables of this type
 		indVarPicker.dataSetChanged(e);
 	}
 
+	/*
+	 * Called whenever a SubspaceEvent is broadcast from a firing bean
+	 * @see geovista.common.event.SubspaceListener#subspaceChanged(geovista.common.event.SubspaceEvent)
+	 */
 	public void subspaceChanged(SubspaceEvent e) {
 
 		// only include subspace events from the local
@@ -236,149 +265,226 @@ public class DiscriminantAnalysisGUI extends JPanel implements ActionListener,
 			setIndVarIndices(e.getSubspace());
 		}
 	}
+	
+	/*
+	 * Methods dealing with sending and receiving selection events
+	 * Sending such events : addSelectionListener and removeSelectionListener
+	 * Receiving such events : selectionChanged and getSelectionEvent (SelectionListener interface)
+	 */
 
+	public void selectionChanged(SelectionEvent e) {
+		map.selectionChanged(e);
+	}
+	
+	public SelectionEvent getSelectionEvent() {
+		return new SelectionEvent(this, map.getSelectedObservations());
+	}
+	
+	public void addSelectionListener(SelectionListener l) {
+		map.addSelectionListener(l);
+	}
+
+	public void removeSelectionListener(SelectionListener l) {
+		map.removeSelectionListener(l);
+	}
+	
+	
+	/*
+	 * Methods dealing with sending and receiving IndicationEvents :
+	 * Sending such events : addIndicationListener & removeIndicationListener
+	 * Receiving such events : indicationChanged (IndicationListener Interface)
+	 */
+	
+	public void indicationChanged(IndicationEvent e) {
+		map.indicationChanged(e);
+	}
+	
+	public void addIndicationListener(IndicationListener l) {
+		map.addIndicationListener(l);
+	}
+
+	public void removeIndicationListener(IndicationListener l) {
+		map.removeIndicationListener(l);
+	}
+	
+	
+	/*
+	 * Perform the classification and write subsequent diagnostics to the screen
+	 */
 	private void classify() {
-
-		if (categoryIndex >= 0 && indVarIndices.length > 0) {
-
-			logger.info("Classification Category : ["
-					+ dataSet.getNumericArrayName(categoryIndex)
-					+ "], numeric index : [" + categoryIndex + "]");
-			logger.info("Independent Variables:");
-			for (int indVarIndice : indVarIndices) {
-				logger.info("[" + dataSet.getNumericArrayName(indVarIndice)
-						+ "], numeric index : [" + indVarIndice + "]");
-			}
-
-			logger.info("Data set contains ["
-					+ String.valueOf(dataSet.getNumObservations())
-					+ "] observations");
-			logger.info("Dataset contains ["
-					+ String.valueOf(dataSet.getColumnCount()) + "] fields");
+				
+		// First, make sure that a category index has been found and that there
+		// is at least one independent variable		
+		if ( categoryIndex >= 0 && indVarIndices.length > 0 ) {
 
 			// the data array consists of arrays of columns
 			double[][] data = new double[indVarIndices.length][0];
 			for (int i = 0; i < indVarIndices.length; i++) {
 				data[i] = dataSet.getNumericDataAsDouble(indVarIndices[i]);
 			}
-
+			
+			// get the category data
 			String categoryName = dataSet.getNumericArrayName(categoryIndex);
 			int[] categories = dataSet.getIntArrayDataByName(categoryName);
-			da.setPredictorVariables(data, false);
-			da.setClassification(categories);
-			da.classify();
+				
+			try {
+				
+				// set the independent variables
+				da.setPredictorVariables(data,false);
+				
+				// set the dependent variable (category)
+				da.setClassification(categories);
 
-			// **********************************************************
-			// get various diagnostics
-			// **********************************************************
-
-			int[] uniqueClasses = da.getUniqueClasses();
-			int[][] confMatrix = da.confusionMatrix();
-			int[] classFreq = da.getClassFrequencies();
-			double[][] params = da.getParameters();
-			int numFields = da.getNumAttributes();
-			int numObs = da.getNumObservations();
-
-			int numClasses = uniqueClasses.length;
-
-			// write out the confusion matrix
-			JTable cMatrixTable = new JTable(numClasses + 2, numClasses + 2);
-			cMatrixTable.setBorder(BorderFactory.createLineBorder(Color.black));
-
-			for (int i = 0; i < numClasses; i++) {
-
-				cMatrixTable.setValueAt(
-						"Class " + String.valueOf(uniqueClasses[i]), i + 1, 0);
-				cMatrixTable.setValueAt(
-						"Class " + String.valueOf(uniqueClasses[i]), 0, i + 1);
-
-				int totalClassified = 0;
-
-				for (int j = 0; j < numClasses; j++) {
-					cMatrixTable.setValueAt(confMatrix[i][j], i + 1, j + 1);
-					totalClassified += confMatrix[j][i];
+				// set the prior probabilities to the default (equal)
+				da.setPriorProbabilities();
+				
+				// classify the data
+				da.classify();
+					
+				// get output data				
+				int[] uniqueClasses = da.getUniqueClasses();			
+				int[][] confMatrix = da.confusionMatrix();
+				int[] classFreq = da.getClassFrequencies();
+				double[][] params = da.getParameters();
+				int numFields = da.getNumAttributes();
+				int numObs = da.getNumObservations();				
+				int numClasses = uniqueClasses.length;
+				
+				// write some basic details about the classification to the screen 
+				//( name of dependent and independent variables etc)
+				JTextArea info = new JTextArea("\nClassification " + Integer.toString(++numClassifications));
+				info.append("\n\nClassification Category : " + categoryName);
+				info.append("\nIndependent Variables (" + Integer.toString(indVarIndices.length) + ") : ");
+				for (int i=0; i < indVarIndices.length; i++) {
+					info.append("\n" + dataSet.getNumericArrayName(indVarIndices[i]) + " (Variable " + 
+								+ Integer.valueOf(indVarIndices[i]) + ")");
 				}
-				cMatrixTable.setValueAt(classFreq[i], i + 1, numClasses + 1);
-				cMatrixTable.setValueAt(totalClassified, numClasses + 1, i + 1);
-			}
-			cMatrixTable.setValueAt("Total by Class", 0, numClasses + 1);
-			cMatrixTable.setValueAt("Total Assigned", numClasses + 1, 0);
-			cMatrixTable.setValueAt(numObs, numClasses + 1, numClasses + 1);
-
-			outputInfo.add(new JLabel("Confusion Matrix"));
-			outputInfo.add(cMatrixTable);
-
-			// write out the classification function coefficients
-			JTable pMatrixTable = new JTable(numFields + 2, numClasses + 1);
-			pMatrixTable.setBorder(BorderFactory.createLineBorder(Color.black));
-
-			for (int i = 0; i < numFields + 1; i++) {
-
-				pMatrixTable.setValueAt("Paramater " + String.valueOf(i),
-						i + 1, 0);
-
-				for (int j = 0; j < numClasses; j++) {
-
-					if (i == 0) {
-						pMatrixTable.setValueAt("Class " + String.valueOf(j),
-								0, j + 1);
+				info.setEditable(false);
+							
+				// write out the confusion matrix & percentages correctly classified
+				JTable cMatrixTable = new JTable(numClasses + 2, numClasses + 2);
+				cMatrixTable.setBorder(BorderFactory.createLineBorder(Color.black));
+				
+				// compute classification accuracy (percentage correctly classified)
+				double classAccuracy = 0.0;
+				
+				// compute classification accuracy rate if classification were done randomly on the basis of the 
+				// observed class frequencies which are used in lieu of prior probabilities
+				double randomClassAccuracy = 0.0;
+							
+				for ( int i = 0; i < numClasses; i++) {
+					cMatrixTable.setValueAt("Class " + String.valueOf(uniqueClasses[i]), i+1, 0);
+					cMatrixTable.setValueAt("Class " + String.valueOf(uniqueClasses[i]), 0, i+1);
+									
+					// count the number of correctly classified observations
+					classAccuracy += confMatrix[i][i];
+					
+					// compute classification accuracy rate if classification were done randomly
+					// on the basis of observed classification frequencies
+					randomClassAccuracy += Math.pow(( (double)classFreq[i] / (double)numObs),2);
+													
+					int totalClassified = 0;
+					
+					for (int j = 0; j < numClasses; j++) {
+						
+						// compute percentage correctly classified for the current cell in the 
+						// confusion matrix
+						double pcc = ( (double)confMatrix[i][j] / (double)classFreq[i] ) * 100.0;
+						
+						cMatrixTable.setValueAt(String.format("%2d (%-3.1f %%)",confMatrix[i][j],pcc), i+1, j+1);
+						totalClassified += confMatrix[j][i];
+						
 					}
-					pMatrixTable.setValueAt(
-							String.format("%.4f", params[i][j]), i + 1, j + 1);
+					cMatrixTable.setValueAt(classFreq[i],i+1,numClasses+1);
+					cMatrixTable.setValueAt(totalClassified, numClasses+1,i+1);
 				}
+				cMatrixTable.setValueAt("Total by Class",0,numClasses+1);
+				cMatrixTable.setValueAt("Total Assigned",numClasses+1,0);
+				cMatrixTable.setValueAt(numObs,numClasses+1,numClasses+1);
+				
+				
+				// compute the classification accuracy and error rate & write to the diagnostics screen
+				classAccuracy = ( classAccuracy / numObs ) * 100;
+				info.append("\n\nClassification Accuracy                      :  " + 
+						String.format("%.2f %%", classAccuracy));
+				info.append("\nClassification Error Rate                    : " + 
+						String.format("%.2f %%", 100.0 - classAccuracy));
+				info.append("\nClassification Accuracy (Random Assignment)  :  " 
+						+ String.format("%.2f %%", randomClassAccuracy * 100));
+						
+				outputInfo.add(info);
+				outputInfo.add(new JLabel("Confusion Matrix - Percentage Correctly Classified"));
+				outputInfo.add(cMatrixTable);
+				
+				// write out the classification function coefficients
+				JTable pMatrixTable = new JTable(numFields + 2, numClasses + 1);
+				pMatrixTable.setBorder(BorderFactory.createLineBorder(Color.black));
+						 			
+				for (int i = 0; i < numFields+1; i++) {
+			
+					pMatrixTable.setValueAt("Paramater " + String.valueOf(i),i+1,0);
+				
+					for (int j = 0; j < numClasses; j++) {
+						
+						if (i == 0) {
+							pMatrixTable.setValueAt("Class " + String.valueOf(j),0,j+1);
+						}
+						pMatrixTable.setValueAt(String.format("%.4f",params[i][j]),i+1,j+1);
+					}
+				}
+				
+				outputInfo.add(new JLabel("Classification Function Paramaters"));
+				outputInfo.add(pMatrixTable);
+				
+				// now send the new data to the geomap in the current bean
+										
+				DataSetBroadcaster dataCaster = new DataSetBroadcaster();
+				
+				String[] fNames = new String[2 + (2*numClasses)];
+				Object[] dataSetChanged = new Object[4 + (2*numClasses)];
+				fNames[0] = "Class";
+				fNames[1] = "Classified";
+				dataSetChanged[1] = da.getClassification();
+				dataSetChanged[2] = da.getClassified();
+				for ( int i = 0; i < numClasses; i++) {
+					fNames[i+2] = "MhDist2_" + String.valueOf(i);
+					dataSetChanged[i+3] = da.getMahalanobisDistance2(i);
+				}
+				for ( int i = 0; i < numClasses; i++) {
+					fNames[i+(2 + numClasses)] = "PostProb_" + String.valueOf(i);
+					dataSetChanged[i+(3+numClasses)] = da.getPosteriorProbabilities(i);
+				}
+				dataSetChanged[0] = fNames;
+				dataSetChanged[3 + (2*numClasses)] = dataSet.getShapeData();
+				
+				
+				dataCaster.addDataSetListener(map);
+				dataCaster.setAndFireDataSet(dataSetChanged);
+				map.repaint();
+				
+			} catch (DiscriminantAnalysisException e ){
+				String message = "Unable to classify : " + e.getMessage();
+				JOptionPane.showMessageDialog(this, message, "WARNING", JOptionPane.WARNING_MESSAGE);
 			}
-
-			outputInfo.add(new JLabel("Classification Function Paramaters"));
-			outputInfo.add(pMatrixTable);
-
-			DataSetBroadcaster dataCaster = new DataSetBroadcaster();
-
-			String[] fNames = new String[2 + (2 * numClasses)];
-			Object[] dataSetChanged = new Object[4 + (2 * numClasses)];
-			fNames[0] = "Class";
-			fNames[1] = "Classified";
-			dataSetChanged[1] = da.getClassification();
-			dataSetChanged[2] = da.getClassified();
-			for (int i = 0; i < numClasses; i++) {
-				fNames[i + 2] = "MhDist2_" + String.valueOf(i);
-				dataSetChanged[i + 3] = da.getMahalanobisDistance2(i);
-			}
-			for (int i = 0; i < numClasses; i++) {
-				fNames[i + (2 + numClasses)] = "PostProb_" + String.valueOf(i);
-				dataSetChanged[i + (3 + numClasses)] = da
-						.getPosteriorProbabilities(i);
-			}
-			dataSetChanged[0] = fNames;
-			dataSetChanged[3 + (2 * numClasses)] = dataSet.getShapeData();
-
-			// String[] fNames = {"Class","Classified","Mh"};
-			// Object[] dataSetChanged = {fNames, da.getClassification(),
-			// da.getClassified(), da.getMahalanobisDistance2(0),
-			// dataSet.getShapeData()};
-
-			// DataSetForApps dataSetChanged = new
-			// DataSetForApps(dataSet.getDataSetFull());
-			// dataSetChanged.addColumn("Classified", da.getClassified());
-			// for (int i = 0; i < numClasses; i++) {
-			// dataSetChanged.addColumn("MhDist2_" + String.valueOf(i),
-			// da.getMahalanobisDistance2(i));
-			// dataSetChanged.addColumn("PostProb_" + String.valueOf(i),
-			// da.getPosteriorProbabilities(i));
-			// }
-
-			dataCaster.addDataSetListener(map);
-			dataCaster.setAndFireDataSet(dataSetChanged);
-			map.repaint();
-
+												
 		} else {
-			logger.info("Unable to run classification");
-			logger.info("Category Index is ["
-					+ ((categoryIndex < 0) ? "not set" : "set") + "]");
-			logger.info("Independent Variables are ["
-					+ ((indVarIndices.length == 0) ? "not set" : "set") + "]");
+			
+			String message = "Unable to classify : ";
+			if ( categoryIndex < 0 ) {
+				message += "\nCategory Index is " + ((categoryIndex < 0) ? "not set" : "set");
+			}
+			
+			if (indVarIndices.length == 0) {
+				message += "\nIndependent Variables are " + ((indVarIndices.length == 0) ? "not set" : "set");
+			}
+			JOptionPane.showMessageDialog(this, message, "WARNING", JOptionPane.WARNING_MESSAGE);
 		}
 	}
 
+	/*
+	 * Set the independent variable indices and check to make sure that there is no overlap between 
+	 * them and the dependent variable
+	 */
 	private void setIndVarIndices(int[] indVarIndices) {
 
 		this.indVarIndices = indVarIndices;
