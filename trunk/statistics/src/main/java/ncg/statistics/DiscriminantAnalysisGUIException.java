6a6309e1ac7aa6780602d6ad78f2f0c82b42df34
peter.foley@gmail.com <peter.foley@gmail.com@9eb86e29-fa2f-0410-8635-6322a288db1e>
2010-09-06 17:30:19 +0000

    Latest changes to DiscriminantAnalysis java bean

---
+++ trunk/statistics/src/main/java/ncg/statistics/DiscriminantAnalysisGUIException.java
@@ -0,0 +1,27 @@
+package ncg.statistics;
+
+/*
+ * Exception thrown by the DiscriminantAnalysisGUI class
+ */
+public class DiscriminantAnalysisGUIException extends Exception {
+
+	// constructor signatures all match constructors of the Exception class
+	
+	public DiscriminantAnalysisGUIException() {
+		super();
+	}
+	
+	public DiscriminantAnalysisGUIException(String message) {
+		super(message);
+	}
+	
+	public DiscriminantAnalysisGUIException(String message, Throwable cause) {
+		super(message,cause);
+	}
+	
+	public DiscriminantAnalysisGUIException(Throwable cause) {
+		super(cause);
+	}
+	
+	
+}
