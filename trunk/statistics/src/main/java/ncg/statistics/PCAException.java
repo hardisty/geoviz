0ab1b2d10449cc45793186b7009057c709056f8e
peter.foley@gmail.com <peter.foley@gmail.com@9eb86e29-fa2f-0410-8635-6322a288db1e>
2010-10-14 17:42:55 +0000

    Add PCA code to NCG Statistics package

---
+++ trunk/statistics/src/main/java/ncg/statistics/PCAException.java
@@ -0,0 +1,27 @@
+package ncg.statistics;
+
+/*
+ * Exception thrown by the PCA class
+ */
+public class PCAException extends Exception {
+
+	// constructor signatures all match constructors of the Exception class
+	
+	public PCAException() {
+		super();
+	}
+	
+	public PCAException(String message) {
+		super(message);
+	}
+	
+	public PCAException(String message, Throwable cause) {
+		super(message,cause);
+	}
+	
+	public PCAException(Throwable cause) {
+		super(cause);
+	}
+	
+	
+}
