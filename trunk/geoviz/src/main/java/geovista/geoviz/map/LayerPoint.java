bcb54e89fbe4aca1bcab1206aa9669c38e8a07a3
frank.hardisty@gmail.com <frank.hardisty@gmail.com@9eb86e29-fa2f-0410-8635-6322a288db1e>
2012-04-02 20:57:46 +0000

    Added new spatial type to DSA Adding metadata object to DataSetEvent still working on colors...

--- trunk/geoviz/src/main/java/geovista/geoviz/map/LayerPoint.java
+++ trunk/geoviz/src/main/java/geovista/geoviz/map/LayerPoint.java
@@ -20,154 +20,159 @@ import geovista.symbolization.glyph.Glyph;
  */
 public class LayerPoint extends LayerShape {
 
-	private Point2D[] originalPoints;
+    private Point2D[] originalPoints;
 
-	@Override
-	public void setGlyphs(Glyph[] glyphs) {
+    @Override
+    public void setGlyphs(Glyph[] glyphs) {
+    }
+
+    /*
+     * SelectionX1 is expected to be less than selectionX2, same with Y1 and y2.
+     * Selected observations should be rendered with the color "colorSelection".
+     */
+    @Override
+    public void findSelection(int selectionX1, int selectionX2,
+	    int selectionY1, int selectionY2) {
+	Rectangle selBox = new Rectangle(selectionX1, selectionY1, selectionX2
+		- selectionX1, selectionY2 - selectionY1);
+
+	Vector selObs = new Vector();
+	for (int i = 0; i < spatialData.length; i++) {
+	    Rectangle shpBox = spatialData[i].getBounds();
+	    if (selBox.intersects(shpBox)) {
+		if (spatialData[i].contains(selBox)
+			|| spatialData[i].intersects(selBox)) {
+		    selObs.add(new Integer(i));
+		} // end if really intersects
+	    } // end if rough intersects
+	} // next
+	selectedObservations = new int[selObs.size()];
+	int j = 0;
+	for (Enumeration e = selObs.elements(); e.hasMoreElements();) {
+	    Integer anInt = (Integer) e.nextElement();
+	    selectedObservations[j] = anInt.intValue();
+	    j++;
 	}
+    }
 
-	/*
-	 * SelectionX1 is expected to be less than selectionX2, same with Y1 and y2.
-	 * Selected observations should be rendered with the color "colorSelection".
-	 */
-	@Override
-	public void findSelection(int selectionX1, int selectionX2,
-			int selectionY1, int selectionY2) {
-		Rectangle selBox = new Rectangle(selectionX1, selectionY1, selectionX2
-				- selectionX1, selectionY2 - selectionY1);
+    /*
+     * selectionX1 is expected to be less than selectionX2, same with Y1 and y2
+     */
+    @Override
+    public void findSelectionShift(int selectionX1, int selectionX2,
+	    int selectionY1, int selectionY2) {
+	Rectangle selBox = new Rectangle(selectionX1, selectionY1, selectionX2
+		- selectionX1, selectionY2 - selectionY1);
 
-		Vector selObs = new Vector();
-		for (int i = 0; i < spatialData.length; i++) {
-			Rectangle shpBox = spatialData[i].getBounds();
-			if (selBox.intersects(shpBox)) {
-				if (spatialData[i].contains(selBox)
-						|| spatialData[i].intersects(selBox)) {
-					selObs.add(new Integer(i));
-				} // end if really intersects
-			} // end if rough intersects
-		} // next
-		selectedObservations = new int[selObs.size()];
-		int j = 0;
-		for (Enumeration e = selObs.elements(); e.hasMoreElements();) {
-			Integer anInt = (Integer) e.nextElement();
-			selectedObservations[j] = anInt.intValue();
-			j++;
+	Vector selObs = new Vector();
+	Arrays.sort(selectedObservations); // have to do this for the
+	// searching
+	for (int i = 0; i < spatialData.length; i++) {
+	    Rectangle shpBox = spatialData[i].getBounds();
+	    if (selBox.intersects(shpBox)) {
+		if (Arrays.binarySearch(selectedObservations, i) < 0) {
+		    selObs.add(new Integer(i));
 		}
+	    }
+	}
+	int[] selectedObserCp = new int[selectedObservations.length];
+	selectedObserCp = (selectedObservations.clone());
+	selectedObservations = new int[selectedObserCp.length + selObs.size()];
+	int j = 0;
+	for (j = 0; j < selectedObserCp.length; j++) {
+	    selectedObservations[j] = selectedObserCp[j];
+	}
+	for (Enumeration e = selObs.elements(); e.hasMoreElements();) {
+	    Integer anInt = (Integer) e.nextElement();
+	    selectedObservations[j] = anInt.intValue();
+	    j++;
 	}
 
-	/*
-	 * selectionX1 is expected to be less than selectionX2, same with Y1 and y2
-	 */
-	@Override
-	public void findSelectionShift(int selectionX1, int selectionX2,
-			int selectionY1, int selectionY2) {
-		Rectangle selBox = new Rectangle(selectionX1, selectionY1, selectionX2
-				- selectionX1, selectionY2 - selectionY1);
+    }
 
-		Vector selObs = new Vector();
-		Arrays.sort(selectedObservations); // have to do this for the
-		// searching
-		for (int i = 0; i < spatialData.length; i++) {
-			Rectangle shpBox = spatialData[i].getBounds();
-			if (selBox.intersects(shpBox)) {
-				if (Arrays.binarySearch(selectedObservations, i) < 0) {
-					selObs.add(new Integer(i));
-				}
-			}
-		}
-		int[] selectedObserCp = new int[selectedObservations.length];
-		selectedObserCp = (selectedObservations.clone());
-		selectedObservations = new int[selectedObserCp.length + selObs.size()];
-		int j = 0;
-		for (j = 0; j < selectedObserCp.length; j++) {
-			selectedObservations[j] = selectedObserCp[j];
-		}
-		for (Enumeration e = selObs.elements(); e.hasMoreElements();) {
-			Integer anInt = (Integer) e.nextElement();
-			selectedObservations[j] = anInt.intValue();
-			j++;
-		}
+    @Override
+    public int findIndication(int x, int y) {
+	for (int i = 0; i < spatialData.length; i++) {
+	    Rectangle shpBox = spatialData[i].getBounds();
+	    if (shpBox.contains(x, y)) {
+		if (spatialData[i].contains(x, y)) {
+		    return i;
 
+		} // end if really intersects
+	    } // end if rough intersects
+	} // next
+	  // couldn't find anything, so
+	return Integer.MIN_VALUE;
+    }
+
+    /**
+     * sets the points that the layer will use to draw from these are assumed to
+     * be in user space
+     * 
+     * @param originalPoints
+     */
+    public void setOriginalPoints(Point2D[] originalPoints) {
+	this.originalPoints = originalPoints;
+    }
+
+    /**
+     * retrieves the points
+     * 
+     * @param originalPoints
+     */
+    public Point2D[] getOriginalPoints() {
+	return originalPoints;
+    }
+
+    public Point2D[] shapesToPoints(Shape[] shapes) {
+	Point2D[] points = new Point2D.Double[shapes.length];
+	for (int i = 0; i < shapes.length; i++) {
+	    Point2D pnt = new Point2D.Double();
+	    Shape shp = shapes[i];
+	    PathIterator path = shp.getPathIterator(null);
+	    double[] seg = new double[6];
+	    int segType = path.currentSegment(seg);
+	    if (segType != PathIterator.SEG_MOVETO) {
+		throw new IllegalArgumentException(
+			"LayerPointExtension.shapesToPoints expects only PathIterator.SEG_MOVETO segments");
+	    }
+	    pnt.setLocation(seg[0], seg[1]);
+	    points[i] = pnt;
 	}
 
-	@Override
-	public int findIndication(int x, int y) {
-		for (int i = 0; i < spatialData.length; i++) {
-			Rectangle shpBox = spatialData[i].getBounds();
-			if (shpBox.contains(x, y)) {
-				if (spatialData[i].contains(x, y)) {
-					return i;
+	return points;
+    }
 
-				} // end if really intersects
-			} // end if rough intersects
-		} // next
-		// couldn't find anything, so
-		return Integer.MIN_VALUE;
+    public Shape[] pointsToShapes(Point2D[] points) {
+	Shape[] shapes = new Shape[points.length];
+	for (int i = 0; i < shapes.length; i++) {
+
+	    GeneralPath shp = new GeneralPath();
+	    Point2D pnt = points[i];
+	    shp.moveTo((float) pnt.getX(), (float) pnt.getY());
+	    Shape circle = new Ellipse2D.Float((float) pnt.getX(),
+		    (float) pnt.getY(), 10, 10);
+
+	    shapes[i] = shp;
+	    shapes[i] = circle;
 	}
 
-	/**
-	 * sets the points that the layer will use to draw from these are assumed to
-	 * be in user space
-	 * 
-	 * @param originalPoints
-	 */
-	public void setOriginalPoints(Point2D[] originalPoints) {
-		this.originalPoints = originalPoints;
+	return shapes;
+    }
+
+    public Shape[] findShapesForPoints(Point2D[] points) {
+	Shape[] shapes = new Shape[points.length];
+	float shapeSize = 8f;
+	float half = shapeSize / 2f;
+	for (int i = 0; i < shapes.length; i++) {
+	    Point2D pt = points[i];
+
+	    Ellipse2D eli = new Ellipse2D.Float((float) pt.getX() - half,
+		    (float) pt.getY() - half, shapeSize, shapeSize);
+	    GeneralPath path = new GeneralPath(eli);
+	    shapes[i] = path;
 	}
 
-	/**
-	 * retrieves the points
-	 * 
-	 * @param originalPoints
-	 */
-	public Point2D[] getOriginalPoints() {
-		return originalPoints;
-	}
-
-	public Point2D[] shapesToPoints(Shape[] shapes) {
-		Point2D[] points = new Point2D.Double[shapes.length];
-		for (int i = 0; i < shapes.length; i++) {
-			Point2D pnt = new Point2D.Double();
-			Shape shp = shapes[i];
-			PathIterator path = shp.getPathIterator(null);
-			double[] seg = new double[6];
-			int segType = path.currentSegment(seg);
-			if (segType != PathIterator.SEG_MOVETO) {
-				throw new IllegalArgumentException(
-						"LayerPointExtension.shapesToPoints expects only PathIterator.SEG_MOVETO segments");
-			}
-			pnt.setLocation(seg[0], seg[1]);
-			points[i] = pnt;
-		}
-
-		return points;
-	}
-
-	public Shape[] pointsToShapes(Point2D[] points) {
-		Shape[] shapes = new Shape[points.length];
-		for (int i = 0; i < shapes.length; i++) {
-			GeneralPath shp = new GeneralPath();
-			Point2D pnt = points[i];
-			shp.moveTo((float) pnt.getX(), (float) pnt.getY());
-			shapes[i] = shp;
-		}
-
-		return shapes;
-	}
-
-	public Shape[] findShapesForPoints(Point2D[] points) {
-		Shape[] shapes = new Shape[points.length];
-		float shapeSize = 8f;
-		float half = shapeSize / 2f;
-		for (int i = 0; i < shapes.length; i++) {
-			Point2D pt = points[i];
-
-			Ellipse2D eli = new Ellipse2D.Float((float) pt.getX() - half,
-					(float) pt.getY() - half, shapeSize, shapeSize);
-			GeneralPath path = new GeneralPath(eli);
-			shapes[i] = path;
-		}
-
-		return shapes;
-	}
+	return shapes;
+    }
 }
