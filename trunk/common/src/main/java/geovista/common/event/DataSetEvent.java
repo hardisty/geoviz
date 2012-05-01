ef6a2cb2ee4296f1e899d59d603fe31d158f7aba
frank.hardisty@gmail.com <frank.hardisty@gmail.com@9eb86e29-fa2f-0410-8635-6322a288db1e>
2012-05-01 18:38:46 +0000

    MetaData, first cut

--- trunk/common/src/main/java/geovista/common/event/DataSetEvent.java
+++ trunk/common/src/main/java/geovista/common/event/DataSetEvent.java
@@ -5,6 +5,7 @@
 package geovista.common.event;
 
 import java.util.EventObject;
+import java.util.HashMap;
 
 import geovista.common.data.DataSetForApps;
 
@@ -14,47 +15,50 @@ import geovista.common.data.DataSetForApps;
  */
 public class DataSetEvent extends EventObject {
 
-	private final Object[] dataSet;
-	private transient DataSetForApps dataSetForApps;
-	private Object metaData;
+    private final Object[] dataSet;
+    private transient DataSetForApps dataSetForApps;
+    private HashMap<String, String> metaData;
 
-	/**
-	 * The constructor is the same as that for EventObject, except that the
-	 * dataSet values are indicated.
-	 */
-	@Deprecated
-	public DataSetEvent(Object source, Object[] dataSet) {
-		super(source);
-		this.dataSet = dataSet;
-	}
+    /**
+     * The constructor is the same as that for EventObject, except that the
+     * dataSet values are indicated.
+     */
+    @Deprecated
+    public DataSetEvent(Object source, Object[] dataSet) {
+	super(source);
+	this.dataSet = dataSet;
+    }
 
-	/**
-	 * Note that the DataSetForApps is first. This allows us to pass in a null
-	 * DataSetForApps if desired, without being confused with the previous cntr.
-	 */
-	public DataSetEvent(DataSetForApps dataSetForApps, Object source) {
-		super(source);
-		this.dataSetForApps = dataSetForApps;
-		dataSet = dataSetForApps.getDataObjectOriginal();
-	}
+    /**
+     * Note that the DataSetForApps is first. This allows us to pass in a null
+     * DataSetForApps if desired, without being confused with the previous cntr.
+     */
+    public DataSetEvent(DataSetForApps dataSetForApps, Object source) {
+	super(source);
+	this.dataSetForApps = dataSetForApps;
+	dataSet = dataSetForApps.getDataObjectOriginal();
+    }
 
-	public Object[] getDataSet() {
-		return dataSet;
-	}
+    public Object[] getDataSet() {
+	return dataSet;
+    }
 
-	public DataSetForApps getDataSetForApps() {
-		// lazily initialize
-		if (dataSetForApps == null) {
-			dataSetForApps = new DataSetForApps(dataSet);
-		}
-		return dataSetForApps;
+    public DataSetForApps getDataSetForApps() {
+	// lazily initialize
+	if (dataSetForApps == null) {
+	    dataSetForApps = new DataSetForApps(dataSet);
 	}
+	return dataSetForApps;
+    }
 
-	public Object getMetaData() {
-		return metaData;
+    public Object getMetaData() {
+	if (metaData == null) {
+	    metaData = new HashMap<String, String>();
 	}
+	return metaData;
+    }
 
-	public void setMetaData(Object metaData) {
-		this.metaData = metaData;
-	}
+    public void setMetaData(HashMap<String, String> metaData) {
+	this.metaData = metaData;
+    }
 }
