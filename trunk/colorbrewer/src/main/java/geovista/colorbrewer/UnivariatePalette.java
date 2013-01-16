2041472d1dee474894427f4c6d8737cea65c2bb0
samstehle <samstehle@9eb86e29-fa2f-0410-8635-6322a288db1e>
2013-01-16 22:33:13 +0000

    When number of colors asked for is greater than ColorBrewer offers, re-cycle instead of padding with black

--- trunk/colorbrewer/src/main/java/geovista/colorbrewer/UnivariatePalette.java
+++ trunk/colorbrewer/src/main/java/geovista/colorbrewer/UnivariatePalette.java
@@ -9,77 +9,77 @@ import java.util.HashMap;
  */
 public final class UnivariatePalette implements Palette1D {
 
-	String name;
-	SequenceType type;
-	HashMap<Integer, Color[]> colorMap;
-	static int minLength = 3;
-	int maxLength;
+    String name;
+    SequenceType type;
+    HashMap<Integer, Color[]> colorMap;
+    static int minLength = 3;
+    int maxLength;
 
-	UnivariatePalette(String name, SequenceType type,
-			HashMap<Integer, Color[]> colorMap, int maxLength) {
-		this.name = name;
-		this.type = type;
-		this.colorMap = colorMap;
-		this.maxLength = maxLength;
+    UnivariatePalette(String name, SequenceType type,
+	    HashMap<Integer, Color[]> colorMap, int maxLength) {
+	this.name = name;
+	this.type = type;
+	this.colorMap = colorMap;
+	this.maxLength = maxLength;
+    }
+
+    UnivariatePalette combine(UnivariatePalette otherPalette) {
+	int max = maxLength;
+	if (otherPalette.maxLength > maxLength) {
+	    max = otherPalette.maxLength;
 	}
-
-	UnivariatePalette combine(UnivariatePalette otherPalette) {
-		int max = maxLength;
-		if (otherPalette.maxLength > maxLength) {
-			max = otherPalette.maxLength;
-		}
-		HashMap<Integer, Color[]> newColorMap = (HashMap<Integer, Color[]>) colorMap
-				.clone();
-		for (Integer key : otherPalette.colorMap.keySet()) {
-			newColorMap.put(key, otherPalette.colorMap.get(key));
-		}
-		return new UnivariatePalette(name, type, newColorMap, max);
+	HashMap<Integer, Color[]> newColorMap = (HashMap<Integer, Color[]>) colorMap
+		.clone();
+	for (Integer key : otherPalette.colorMap.keySet()) {
+	    newColorMap.put(key, otherPalette.colorMap.get(key));
 	}
+	return new UnivariatePalette(name, type, newColorMap, max);
+    }
 
-	public Color[] getColors(int length) {
+    public Color[] getColors(int length) {
 
-		if (length < 0) {
-			throw new IllegalArgumentException(
-					"can't ask for a negative number of colors ");
-		}
-		Color[] colors = null;
-		if (length == 0) {
-			colors = new Color[0];
-		} else if (length == 1) {
-			colors = new Color[1];
-			colors[0] = colorMap.get(minLength)[0];
-		} else if (length == 2) {
-			colors = new Color[2];
-			colors[0] = colorMap.get(minLength)[0];
-			colors[1] = colorMap.get(minLength)[1];
-		} else if (length <= maxLength) {
-			colors = colorMap.get(length);
-		} else {
-			colors = new Color[length];
-			Color[] brewerColors = colorMap.get(maxLength);
-			for (int i = 0; i < colors.length; i++) {
-				if (i < brewerColors.length) {
-					colors[i] = brewerColors[i];
-				} else {
-					colors[i] = Color.black;
-				}
-			}
-
-		}
-		assert (colors.length == length);
-		return colors;
+	if (length < 0) {
+	    throw new IllegalArgumentException(
+		    "can't ask for a negative number of colors ");
 	}
+	Color[] colors = null;
+	if (length == 0) {
+	    colors = new Color[0];
+	} else if (length == 1) {
+	    colors = new Color[1];
+	    colors[0] = colorMap.get(minLength)[0];
+	} else if (length == 2) {
+	    colors = new Color[2];
+	    colors[0] = colorMap.get(minLength)[0];
+	    colors[1] = colorMap.get(minLength)[1];
+	} else if (length <= maxLength) {
+	    colors = colorMap.get(length);
+	} else {
+	    colors = new Color[length];
+	    Color[] brewerColors = colorMap.get(maxLength);
+	    for (int i = 0; i < colors.length; i++) {
+		// if (i < brewerColors.length) {
+		colors[i] = brewerColors[i % brewerColors.length];
+		// } else {
+		// colors[i] = Color.black;
+		// }
+	    }
 
-	public SequenceType getType() {
-		return type;
 	}
+	assert (colors.length == length);
+	return colors;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public SequenceType getType() {
+	return type;
+    }
 
-	public int getMaxLength() {
-		return maxLength;
-	}
+    public String getName() {
+	return name;
+    }
+
+    public int getMaxLength() {
+	return maxLength;
+    }
 
 }
