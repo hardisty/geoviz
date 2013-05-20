5392cc1a11c77f7d02da37f9ad9079aa9e72bf1f
frank.hardisty <frank.hardisty@gmail.com>
2013-05-20 19:21:54 +0000

    less verbose

--- common/src/main/java/geovista/common/data/SpatialWeights.java
+++ common/src/main/java/geovista/common/data/SpatialWeights.java
@@ -16,96 +16,96 @@ import com.vividsolutions.jts.geom.TopologyException;
 
 public class SpatialWeights {
 
-	ArrayList<ArrayList<WeightedNeighbor>> neighbors;
-	protected final static Logger logger = Logger
-			.getLogger(SpatialWeights.class.getName());
+    ArrayList<ArrayList<WeightedNeighbor>> neighbors;
+    protected final static Logger logger = Logger
+	    .getLogger(SpatialWeights.class.getName());
 
-	public SpatialWeights(int listLength) {
-		super();
-		neighbors = new ArrayList<ArrayList<WeightedNeighbor>>();
-		for (int i = 0; i < listLength; i++) {
-			neighbors.add(new ArrayList<WeightedNeighbor>());
-		}
-
+    public SpatialWeights(int listLength) {
+	super();
+	neighbors = new ArrayList<ArrayList<WeightedNeighbor>>();
+	for (int i = 0; i < listLength; i++) {
+	    neighbors.add(new ArrayList<WeightedNeighbor>());
 	}
 
-	public List<Integer> getNeighborIDs(int obs) {
-		ArrayList<WeightedNeighbor> bors = neighbors.get(obs);
-		ArrayList<Integer> borIds = new ArrayList(bors.size());
-		for (WeightedNeighbor bor : bors) {
-			borIds.add(bor.getTo());
-		}
-		return borIds;
+    }
+
+    public List<Integer> getNeighborIDs(int obs) {
+	ArrayList<WeightedNeighbor> bors = neighbors.get(obs);
+	ArrayList<Integer> borIds = new ArrayList(bors.size());
+	for (WeightedNeighbor bor : bors) {
+	    borIds.add(bor.getTo());
 	}
+	return borIds;
+    }
 
-	public List<Double> getWeights(int obs) {
-		ArrayList<WeightedNeighbor> bors = neighbors.get(obs);
-		ArrayList<Double> weights = new ArrayList(bors.size());
-		for (WeightedNeighbor bor : bors) {
-			weights.add(bor.getWeight());
-		}
-		return weights;
+    public List<Double> getWeights(int obs) {
+	ArrayList<WeightedNeighbor> bors = neighbors.get(obs);
+	ArrayList<Double> weights = new ArrayList(bors.size());
+	for (WeightedNeighbor bor : bors) {
+	    weights.add(bor.getWeight());
 	}
+	return weights;
+    }
 
-	public List<WeightedNeighbor> getWeightedNeighbors(int obs) {
-		return neighbors.get(obs);
+    public List<WeightedNeighbor> getWeightedNeighbors(int obs) {
+	return neighbors.get(obs);
+    }
+
+    private void addNeighbor(int id1, int id2) {
+	if (id1 == id2) {
+	    return;
 	}
+	List<WeightedNeighbor> id1Neighbors = getWeightedNeighbors(id1);
+	List<WeightedNeighbor> id2Neighbors = getWeightedNeighbors(id2);
+	if (id1Neighbors == null) {
+	    id1Neighbors = new ArrayList<WeightedNeighbor>();
+	}
+	if (id2Neighbors == null) {
+	    id2Neighbors = new ArrayList<WeightedNeighbor>();
+	}
+	WeightedNeighbor one = new WeightedNeighbor(id1, id2, 1f);
+	WeightedNeighbor two = new WeightedNeighbor(id2, id1, 1f);
+	id1Neighbors.add(one);
+	id2Neighbors.add(two);
 
-	private void addNeighbor(int id1, int id2) {
-		if (id1 == id2) {
-			return;
-		}
-		List<WeightedNeighbor> id1Neighbors = getWeightedNeighbors(id1);
-		List<WeightedNeighbor> id2Neighbors = getWeightedNeighbors(id2);
-		if (id1Neighbors == null) {
-			id1Neighbors = new ArrayList<WeightedNeighbor>();
-		}
-		if (id2Neighbors == null) {
-			id2Neighbors = new ArrayList<WeightedNeighbor>();
-		}
-		WeightedNeighbor one = new WeightedNeighbor(id1, id2, 1f);
-		WeightedNeighbor two = new WeightedNeighbor(id2, id1, 1f);
-		id1Neighbors.add(one);
-		id2Neighbors.add(two);
+    }
+
+    public Collection<ArrayList<WeightedNeighbor>> findUniqueTopologies() {
+
+	// current (simplest) case: single neighbors
+	HashMap<Integer, ArrayList<WeightedNeighbor>> results = new HashMap<Integer, ArrayList<WeightedNeighbor>>();
+	for (ArrayList<WeightedNeighbor> weights : neighbors) {
+	    results.put(weights.size(), weights);
 
 	}
+	return results.values();
+    }
 
-	public Collection<ArrayList<WeightedNeighbor>> findUniqueTopologies() {
+    public void findNeighbors(List<Geometry> geoms) {
+	long nTouches = 0;
+	for (int i = 0; i < geoms.size() - 1; i++) {
+	    Geometry geom = geoms.get(i);
 
-		// current (simplest) case: single neighbors
-		HashMap<Integer, ArrayList<WeightedNeighbor>> results = new HashMap<Integer, ArrayList<WeightedNeighbor>>();
-		for (ArrayList<WeightedNeighbor> weights : neighbors) {
-			results.put(weights.size(), weights);
-
+	    for (int j = i + 1; j < geoms.size(); j++) {
+		Geometry geom2 = geoms.get(j);
+		boolean touches = false;
+		try {
+		    touches = geom.touches(geom2);
+		    if (logger.isLoggable(Level.FINEST)) {
+			logger.finest("" + touches);
+		    }
+		} catch (TopologyException te) {
+		    logger.fine("buffing " + i + " and " + j);
+		    geom = geom.buffer(0);
+		    geom2 = geom2.buffer(0);
+		    touches = geom.touches(geom2);
 		}
-		return results.values();
-	}
-
-	public void findNeighbors(List<Geometry> geoms) {
-		long nTouches = 0;
-		for (int i = 0; i < geoms.size() - 1; i++) {
-			Geometry geom = geoms.get(i);
-
-			for (int j = i + 1; j < geoms.size(); j++) {
-				Geometry geom2 = geoms.get(j);
-				boolean touches = false;
-				try {
-					touches = geom.touches(geom2);
-					if (logger.isLoggable(Level.FINEST)) {
-						logger.finest("" + touches);
-					}
-				} catch (TopologyException te) {
-					logger.info("buffing " + i + " and " + j);
-					geom = geom.buffer(0);
-					geom2 = geom2.buffer(0);
-					touches = geom.touches(geom2);
-				}
-				if (geom.touches(geom2)) {
-					addNeighbor(i, j);
-					nTouches++;
-				}
-			}// next j
-		}// next i
-		logger.info("Number of touches = " + nTouches);
-	}
+		if (geom.touches(geom2)) {
+		    addNeighbor(i, j);
+		    nTouches++;
+		}
+	    }// next j
+	}// next i
+	logger.fine("Number of touches = " + nTouches);
+    }
 }
